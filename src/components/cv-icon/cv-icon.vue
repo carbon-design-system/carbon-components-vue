926b98e06347818c016ead34ec2c014ab93839b6
lee-chase <lee.chase@uk.ibm.com>
2018-10-26 17:08:23 +0100

    feat(component): add cv-icon

---
+++ src/components/cv-icon/cv-icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg class="cv-icon">
+    <use :xlink:href="iconHref"></use>
+  </svg>
+</template>
+
+<script>
+const spritePath = require('carbon-icons/dist/carbon-icons.svg');
+
+export default {
+  name: 'CvIcon',
+  props: {
+    href: { type: String, required: true },
+  },
+  computed: {
+    iconHref() {
+      if (this.href.toLowerCase().startsWith('cv(')) {
+        return `${spritePath}#${this.href.substring(3, this.href.length - 1)}`;
+      }
+      return this.href;
+    },
+  },
+};
+</script>
