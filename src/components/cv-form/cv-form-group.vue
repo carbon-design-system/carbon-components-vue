015870e001d36efd096d8bb2a069f00625afd287
Lee Chase <leechase@live.com>
2018-10-27 17:55:14 +0100

    feat(component): add form wrapper components (#15)

---
+++ src/components/cv-form/cv-form-group.vue
@@ -0,0 +1,23 @@
+<template>
+  <fieldset class="bx--fieldset">
+    <legend class="bx--label">
+      <slot name="label">
+
+      </slot>
+    </legend>
+    <slot name="content">
+
+    </slot>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'CvFormGroup',
+};
+</script>
+
+<style lang="scss">
+// Import Style Definitions
+@import '~carbon-components/scss/components/form/form';
+</style>
