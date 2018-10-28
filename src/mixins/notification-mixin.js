1350a73b81d74be7abaf7edd2feb9b466333adad
Lee Chase <leechase@live.com>
2018-10-28 16:17:41 +0000

    feat(component): add inline notification (#16)

---
+++ src/mixins/notification-mixin.js
@@ -0,0 +1,31 @@
+import { Notification } from 'carbon-components';
+
+const kinds = ['error', 'warning', 'info', 'success'];
+
+export default {
+  props: {
+    kind: {
+      type: String,
+      required: true,
+      validator: value => kinds.indexOf(value) !== -1,
+    },
+    title: String,
+    subTitle: String,
+  },
+  computed: {
+    useAlert() {
+      return kinds.indexOf(this.kind) < 2;
+    },
+  },
+  data() {
+    return {
+      carbonComponent: null,
+    };
+  },
+  mounted() {
+    this.carbonComponent = Notification.create(this.$el);
+  },
+  beforeDestroy() {
+    this.carbonComponent.release();
+  },
+};
