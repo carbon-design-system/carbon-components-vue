6553e6b3a47c24d99a7d5d614dc0d31ed2b35835
Lee Chase <leechase@live.com>
2018-10-29 15:07:35 +0000

    feat(component): add overflow menu (#25)

---
+++ src/components/cv-overflow-menu/cv-overflow-menu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div data-overflow-menu tabindex="0" :aria-label="label" class="bx--overflow-menu cv-overflow-menu">
+    <svg class="bx--overflow-menu__icon" width="3" height="15" viewBox="0 0 3 15">
+      <g fill-rule="evenodd">
+        <circle cx="1.5" cy="1.5" r="1.5" />
+        <circle cx="1.5" cy="7.5" r="1.5" />
+        <circle cx="1.5" cy="13.5" r="1.5" />
+      </g>
+    </svg>
+    <ul
+      class="bx--overflow-menu-options"
+      :class="{
+        'bx--overflow-menu--flip': flipMenu
+      }"
+      tabindex="-1">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { OverflowMenu } from 'carbon-components';
+
+export default {
+  name: 'CvOverflowMenu',
+  props: {
+    label: String,
+    flipMenu: Boolean,
+    offset: {
+      type: Object,
+      validator(value) {
+        return value.hasOwnProperty('left') && value.hasOwnProperty('top');
+      },
+    },
+  },
+  data() {
+    return {
+      carbonComponent: null,
+    };
+  },
+  mounted() {
+    let options = {};
+    if (this.offset) {
+      if (this.flipMenu) {
+        options.objMenuOffsetFlip = this.offset;
+      } else {
+        options.objMenuOffset = this.offset;
+      }
+    }
+
+    this.carbonComponent = OverflowMenu.create(this.$el, options);
+  },
+  beforeDestroy() {
+    this.carbonComponent.release();
+  },
+};
+</script>
+
+<style lang="scss">
+// Import Style Definitions
+@import '~carbon-components/scss/components/overflow-menu/overflow-menu';
+</style>
