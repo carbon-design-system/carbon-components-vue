4804dd812c63207a782078a95f4eea14b3270938
lee-chase <lee.chase@uk.ibm.com>
2018-10-29 17:28:49 +0000

    feat(component): add tiles

---
+++ src/components/cv-tile/cv-tile.vue
@@ -0,0 +1,60 @@
+<template>
+  <component :is="tagType"
+    class="bx--tile"
+    :checked="selected"
+    :expanded="expanded"
+    v-bind="$attrs"
+    >
+    <template><slot></slot></template>
+    <template slot="below"><slot name="below"></slot></template>
+  </component>
+</template>
+
+<script>
+import CvTileClickable from './_cv-tile-clickable';
+import CvTileExpandable from './_cv-tile-expandable';
+import CvTileSelectable from './_cv-tile-selectable';
+import CvTileStandard from './_cv-tile-standard';
+
+export default {
+  name: 'CvTile',
+  inheritAttrs: false,
+  components: {
+    CvTileClickable,
+    CvTileExpandable,
+    CvTileSelectable,
+    CvTileStandard,
+  },
+  props: {
+    expanded: Boolean,
+    selected: Boolean,
+    kind: {
+      type: String,
+      default: '',
+      validator: value =>
+        ['clickable', 'expandable', 'selectable', 'standard', ''].indexOf(
+          value
+        ) !== -1,
+    },
+  },
+  computed: {
+    tagType() {
+      switch (this.kind) {
+        case 'clickable':
+          return 'cv-tile-clickable';
+        case 'selectable':
+          return 'cv-tile-selectable';
+        case 'expandable':
+          return 'cv-tile-expandable';
+        default:
+          return 'cv-tile-standard';
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+// Import Style Definitions
+@import '~carbon-components/scss/components/tile/tile';
+</style>
