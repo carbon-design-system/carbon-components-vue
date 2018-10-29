94008e5a652fee814064a3dd190b11265bfa0a02
lee-chase <lee.chase@uk.ibm.com>
2018-10-29 15:06:55 +0000

    feat(component): add progress component

---
+++ src/components/cv-progress/_cv-progress-step-incomplete.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <svg>
+    <circle cx="12" cy="12" r="12"></circle>
+    <circle v-if="props.isCurrent" cx="12" cy="12" r="6"></circle>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'CvProgressStepIncomplete',
+  props: {
+    isCurrent: Boolean,
+  },
+};
+</script>
