7517ce1e1f677a82c0b3c7457311bf5606a2a0e6
lee-chase <lee.chase@uk.ibm.com>
2018-10-29 17:04:44 +0000

    feat(component): add structured list

---
+++ src/components/cv-structured-list/cv-structured-list-data.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="bx--structured-list-td"
+    :class="{
+      'bx--structured-list-content--nowrap': noWrap,
+    }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CvStructuredListData',
+  props: {
+    noWrap: Boolean,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/cv-structured-list/cv-structured-list-heading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bx--structured-list-th">
+    <slot>
+
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CvStructuredListHeading',
+};
+</script>
+
+<style lang="scss">
+</style>
