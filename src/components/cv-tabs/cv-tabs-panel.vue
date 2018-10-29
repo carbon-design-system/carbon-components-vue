527a6ba0cea7246018b02d4d09fe2dd5be439f36
Lee Chase <leechase@live.com>
2018-10-29 17:19:20 +0000

    feat(component): add tabs (#32)

---
+++ src/components/cv-tabs/cv-tabs-panel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :id="id" role="tabpanel" :aria-labelledby="tabsButtonId" :aria-hidden="!selected" :hidden="!selected">
+    <slot>
+      <!-- Content for first tab goes here. -->
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CvTabsPanel',
+  props: {
+    id: { type: String, required: true },
+    selected: Boolean,
+    tabsButtonId: { type: String, requried: true },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
