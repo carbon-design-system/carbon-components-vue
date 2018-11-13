43f3b55996c1a320f56c098eae911fce7989ef6f
Lee Chase <leechase@live.com>
2018-11-13 17:42:11 +0000

    chore(documentation): add docs, links to issues and repo (#55)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
 import { configure } from '@storybook/vue';
+import { setOptions } from '@storybook/addon-options';
 
 import Vue from 'vue';
 import VueHighlightJS from 'vue-highlightjs';
 
 import CarbonComponentsVue from '../src';
+
+setOptions({
+  name: `carbon components vue`,
+  url: 'https://github.com/carbon-design-system/carbon-components-vue',
+});
 
 Vue.use(CarbonComponentsVue);
 Vue.use(VueHighlightJS);
