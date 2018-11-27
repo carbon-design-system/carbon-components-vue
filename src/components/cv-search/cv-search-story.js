491b9363715713f655db32158bc15771eca6edac
Lee Chase <lee.chase@uk.ibm.com>
2018-11-27 14:41:09 +0000

    chore(story): simplify

--- src/components/cv-search/cv-search-story.js
+++ src/components/cv-search/cv-search-story.js
@@ -5,6 +5,7 @@ import { withNotes } from '@storybook/addon-notes';
 
 import SvTemplateView from '../../views/sv-template-view/sv-template-view';
 import consts from '../../utils/storybook-consts';
+import knobsHelper from '../../utils/storybook-knobs-helper';
 
 import CvSearchNotesMD from './cv-search-notes.md';
 import CvSearch from './cv-search';
@@ -12,74 +13,97 @@ import CvSearch from './cv-search';
 const stories = storiesOf('CvSearch', module);
 stories.addDecorator(withKnobs);
 
-const knobs = () => ({
-  light: boolean('light-theme', false, consts.CONFIG)
-    ? '\n  theme="light"'
-    : '',
-  label: text('label', 'Search label', consts.CONTENT),
-  placeholder: text('placeholder', 'Search placeholder', consts.CONFIG),
-  disabled: boolean('disabled', false, consts.CONFIG) ? '\n  disabled' : '',
-  large: boolean('large', false, consts.CONFIG) ? '\n large' : '',
-  vModel: boolean('v-model', false, consts.OTHER)
-    ? '\n  v-model="modelValue"'
-    : '',
-  otherAttributes: text('other attributes', '', consts.OTHER),
-});
+const kinds = null;
+const preKnobs = {
+  light: {
+    group: 'attr',
+    type: boolean,
+    config: ['light-theme', false, consts.CONFIG],
+    value: val => (val ? '\n  theme="light"' : ''),
+  },
+  label: {
+    group: 'attr',
+    type: text,
+    config: ['label', 'Search label', consts.CONTENT],
+    value: val => (val.length ? `\n  label="${val}"` : ''),
+  },
+  placeholder: {
+    group: 'attr',
+    type: text,
+    config: ['placeholder', 'Search placeholder', consts.CONFIG],
+    value: val => (val.length ? `\n  placeholder="${val}"` : ''),
+  },
+  disabled: {
+    group: 'attr',
+    type: boolean,
+    config: ['disabled', false, consts.CONFIG],
+    value: val => (val ? '\n  disabled' : ''),
+  },
+  large: {
+    group: 'attr',
+    type: boolean,
+    config: ['large', false, consts.CONFIG],
+    value: val => (val ? '\n  large' : ''),
+  },
+  vModel: {
+    group: 'attr',
+    type: boolean,
+    config: ['v-model', false, consts.OTHER],
+    value: val => (val ? '\n  v-model="modelValue"' : '\n  @input="onInput"'),
+  },
+  otherAttributes: {
+    group: 'attr',
+    type: text,
+    config: ['other attributes', '', consts.OTHER],
+    value: val => (val.length ? `\n  ${val}` : ''),
+  },
+};
 
-stories.add(
-  'All',
-  withNotes(CvSearchNotesMD)(() => {
-    const settings = knobs();
+const storySet = knobsHelper.getStorySet(kinds, preKnobs);
 
-    settings.label = settings.label.length ? `${settings.label}` : '';
-    settings.placeholder = settings.placeholder.length
-      ? `\n placeholder="${settings.placeholder}"`
-      : '';
-    settings.listeners = !settings.vModel.includes('v-model')
-      ? '\n   @input="onInput"'
-      : '';
-    settings.otherAttributes = settings.otherAttributes
-      ? `\n  ${settings.otherAttributes}`
-      : '';
+for (const story of storySet) {
+  stories.add(
+    story.name,
+    withNotes(CvSearchNotesMD)(() => {
+      const settings = story.knobs();
 
-    // ----------------------------------------------------------------
-
-    const templateString = `
-<cv-search${settings.large}${settings.disabled}${settings.vModel}${
-      settings.placeholder
-    }${settings.light}${settings.otherAttributes}
-  label="${settings.label}" ${settings.listeners}>
+      // ----------------------------------------------------------------
+      console.dir(settings);
+      const templateString = `
+<cv-search${settings.group.attr}>
 </cv-search>
   `;
+      console.log(templateString);
 
-    // ----------------------------------------------------------------
+      // ----------------------------------------------------------------
 
-    const templateViewString = `
+      const templateViewString = `
     <sv-template-view
       sv-margin
-      :sv-alt-back="light"
+      :sv-alt-back="!light"
       sv-source='${templateString.trim()}'>
       <template slot="component">${templateString}</template>
       <template slot="other">
-        <span class="v-model-example" v-if="${settings.vModel.includes(
-          'v-model'
-        )}">Model value: {{modelValue}}</span>
+        <span class="v-model-example" v-if="${
+          settings.raw.vModel
+        }">Model value: {{modelValue}}</span>
       </template>
     </sv-template-view>
   `;
 
-    return {
-      data() {
-        return {
-          modelValue: '',
-          light: settings.light.length === 0,
-        };
-      },
-      components: { CvSearch, SvTemplateView },
-      template: templateViewString,
-      methods: {
-        onInput: action('cv-search - input event'),
-      },
-    };
-  })
-);
+      return {
+        data() {
+          return {
+            modelValue: '',
+            light: settings.raw.light,
+          };
+        },
+        components: { CvSearch, SvTemplateView },
+        template: templateViewString,
+        methods: {
+          onInput: action('cv-search - input event'),
+        },
+      };
+    })
+  );
+}
