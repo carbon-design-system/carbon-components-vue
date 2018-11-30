7ad2803bc454029872f1520d22292253184ba20c
Lee Chase <leechase@live.com>
2018-11-30 17:11:16 +0000

    Update cv-dropdown-item.vue

--- src/components/cv-dropdown/cv-dropdown-item.vue
+++ src/components/cv-dropdown/cv-dropdown-item.vue
@@ -10,29 +10,33 @@
 -->
 
 <template>
-  <li data-option
-      :data-value="value"
-      class="cv-dropdown-item bx--dropdown-item"
-      v-on:click="selectValue">
-    <a class="bx--dropdown-link" href="javascript:void(0)"><slot></slot></a>
+  <li
+    data-option
+    :data-value="value"
+    class="cv-dropdown-item bx--dropdown-item"
+    v-on:click="selectValue"
+  >
+    <a class="bx--dropdown-link" href="javascript:void(0)">
+      <slot></slot>
+    </a>
   </li>
 </template>
 
 <script>
-  export default {
-    name: 'CvDropdownItem',
-    props: {
-      value: {
-        type: String,
-        required: true
+export default {
+  name: 'CvDropdownItem',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    selectValue: function(event) {
+      if (event) {
+        this.$parent.$emit('change', this.value);
       }
     },
-    methods: {
-      selectValue: function(event) {
-        if (event) {
-          this.$parent.$emit('change', this.value);
-        }
-      }
-    }
-  };
+  },
+};
 </script>
