4344753a8424e48e9c506ad9e6bc85699ac555b7
moores2 <>
2018-12-04 10:56:29 +0000

    fix: add div's with bx--form-item where missing

--- src/components/cv-number-input/cv-number-input.vue
+++ src/components/cv-number-input/cv-number-input.vue
@@ -1,33 +1,35 @@
 <template>
-  <div data-numberinput class="cv-number-input bx--number"
-    :class="{
-      'bx--number--light': theme === 'light',
-      'bx--number--helpertext': $slots['helper-text']
-    }" :data-invalid="invalid">
-    <div class="bx--number__controls">
-      <button class="bx--number__control-btn up-icon">
-        <svg width="10" height="5" viewBox="0 0 10 5">
-          <path d="M0 5L5 .002 10 5z" fill-rule="evenodd" />
-        </svg>
-      </button>
-      <button class="bx--number__control-btn down-icon">
-        <svg width="10" height="5" viewBox="0 0 10 5">
-          <path d="M0 0l5 4.998L10 0z" fill-rule="evenodd" />
-        </svg>
-      </button>
-    </div>
-    <input
-      :id="uid"
-      type="number"
-      :value="value"
-      v-bind="$attrs"
-      v-on="inputListeners">
-    <label :for="uid" class="bx--label">{{label}}</label>
-    <div class="bx--form-requirement" v-if="$slots['invalid-message']">
-      <slot name="invalid-message">Invalid number</slot>
-    </div>
-    <div class="bx--form__helper-text" v-if="$slots['helper-text']">
-      <slot name="helper-text"></slot>
+  <div class="bx--form-item">
+    <div data-numberinput class="cv-number-input bx--number"
+      :class="{
+        'bx--number--light': theme === 'light',
+        'bx--number--helpertext': $slots['helper-text']
+      }" :data-invalid="invalid">
+      <div class="bx--number__controls">
+        <button class="bx--number__control-btn up-icon">
+          <svg width="10" height="5" viewBox="0 0 10 5">
+            <path d="M0 5L5 .002 10 5z" fill-rule="evenodd" />
+          </svg>
+        </button>
+        <button class="bx--number__control-btn down-icon">
+          <svg width="10" height="5" viewBox="0 0 10 5">
+            <path d="M0 0l5 4.998L10 0z" fill-rule="evenodd" />
+          </svg>
+        </button>
+      </div>
+      <input
+        :id="uid"
+        type="number"
+        :value="value"
+        v-bind="$attrs"
+        v-on="inputListeners">
+      <label :for="uid" class="bx--label">{{label}}</label>
+      <div class="bx--form-requirement" v-if="$slots['invalid-message']">
+        <slot name="invalid-message">Invalid number</slot>
+      </div>
+      <div class="bx--form__helper-text" v-if="$slots['helper-text']">
+        <slot name="helper-text"></slot>
+      </div>
     </div>
   </div>
 </template>
