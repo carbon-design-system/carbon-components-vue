88b03a59263c98b147547c7466c212cf6515bdda
Lee Chase <leechase@live.com>
2018-12-19 14:05:49 +0000

    chore: create radio mixin (#92)

--- src/components/cv-structured-list/cv-structured-list.vue
+++ src/components/cv-structured-list/cv-structured-list.vue
@@ -36,8 +36,8 @@ export default {
     // listen children to raise change events
     for (let child of this.$children) {
       if (child.constructor.options.name === 'CvStructuredListItemSelectable') {
-        child.$on('change', ev => {
-          this.$emit('change', ev.target.value);
+        child.$on('change', val => {
+          this.$emit('change', val);
         });
       }
     }
