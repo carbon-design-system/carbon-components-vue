3891d80ac7833a7e99c23870b27e34a894dd6ecf
Lee Chase <lee.chase@uk.ibm.com>
2019-01-02 13:00:14 +0000

    chore: prettier fixes

--- src/components/cv-select/cv-select.vue
+++ src/components/cv-select/cv-select.vue
@@ -76,23 +76,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.bx--visually-hidden {
-  visibility: visible;
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  padding: 0;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  border: 0;
-  white-space: nowrap;
-}
-
-.cv-select {
-  // todo carbon issues? test in date picker
-  display: inline-flex;
-  flex: initial;
-}
-</style>
+<style lang="scss"></style>
