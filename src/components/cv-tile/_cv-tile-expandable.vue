65f10222ff20fec3aef3aae1b88848d85cf33d35
Lee Chase <lee.chase@uk.ibm.com>
2019-01-14 11:53:25 +0000

    fix: fix expandable tile and make it native vue

--- src/components/cv-tile/_cv-tile-expandable.vue
+++ src/components/cv-tile/_cv-tile-expandable.vue
@@ -3,18 +3,23 @@
     data-tile="expandable"
     class="cv-tile-expandable bx--tile--expandable"
     tabindex="0"
-    :class="[{ 'bx--tile--is-expanded': expanded }]"
+    :class="[{ 'bx--tile--is-expanded': internalExpanded }]"
+    :style="styleObject"
   >
-    <button class="bx--tile__chevron">
+    <button class="bx--tile__chevron" @click="toggle">
       <svg width="12" height="8" viewBox="0 0 12 8" fill-rule="evenodd">
         <path d="M10.6 0L6 4.7 1.4 0 0 1.4l6 6.1 6-6.1z"></path>
       </svg>
     </button>
     <div class="bx--tile-content">
-      <span data-tile-atf class="bx--tile-content__above-the-fold">
+      <span
+        data-tile-atf
+        class="bx--tile-content__above-the-fold"
+        ref="aboveFold"
+      >
         <slot> <!-- Above the fold content here --> </slot>
       </span>
-      <span class="bx--tile-content__below-the-fold">
+      <span class="bx--tile-content__below-the-fold" ref="belowFold">
         <slot name="below"> <!-- Rest of the content here --> </slot>
       </span>
     </div>
@@ -29,19 +34,45 @@ export default {
   props: {
     expanded: Boolean,
   },
-  mounted() {
-    this.carbonComponent = Tile.create(this.$el);
-
-    try {
-      // Tile does not resize correctly if initially expanded
-      // Issue - https://github.com/carbon-design-system/carbon-components/issues/781
-      this.carbonComponent._setTileHeight();
-    } catch (e) {
-      console.warn('cv-tile-expandable: Unable to set tile height');
-    }
+  data() {
+    return {
+      styleObject: {
+        maxHeight: 'initial',
+      },
+      internalExpanded: this.expanded,
+    };
   },
-  beforeDestroy() {
-    this.carbonComponent.release();
+  watch: {
+    expanded(val) {
+      if (val !== this.internalExpanded) {
+        this.toggle(val);
+      }
+    },
+  },
+  mounted() {
+    let currentHeight = this.$el.getBoundingClientRect().height;
+    if (!this.expanded) {
+      currentHeight -= this.$refs.belowFold.getBoundingClientRect().height;
+    }
+    this.styleObject.maxHeight = `${currentHeight}px`;
+  },
+  methods: {
+    toggle(force) {
+      const forceType = typeof force;
+      this.internalExpanded =
+        forceType === 'boolean' ? force : !this.internalExpanded;
+
+      const belowFoldHeight = this.$refs.belowFold.getBoundingClientRect()
+        .height;
+      let currentHeight = this.$el.getBoundingClientRect().height;
+
+      if (this.internalExpanded) {
+        currentHeight += belowFoldHeight;
+      } else {
+        currentHeight -= belowFoldHeight;
+      }
+      this.styleObject.maxHeight = `${currentHeight}px`;
+    },
   },
 };
 </script>
