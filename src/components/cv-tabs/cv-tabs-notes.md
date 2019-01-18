3634c14c5d5efc57e6be061b087eb9f93de9ebd4
Lee Chase <leechase@live.com>
2019-01-18 22:20:39 +0000

    chore: make tabs native and follow carbon-react pattern (#120)
    * feat: simplify tabs - like carbon react - and make native Vue
    
    * chore: update readme to match new tabs style

--- src/components/cv-tabs/cv-tabs-notes.md
+++ src/components/cv-tabs/cv-tabs-notes.md
@@ -7,95 +7,29 @@ http://www.carbondesignsystem.com/components/tabs/code
 ## Usage
 
 ```html
-<cv-tabs
-  @tab-selected="actionSelected"
-  @tab-beingselected="actionBeingSelected"
->
-  <cv-tabs-button
-    id="tabs-button-1"
-    content-selector="#tabs-panel-1"
-    tabs-panel-id="tabs-panel-5"
-    selected
-  >
-    Tab label 1
-  </cv-tabs-button>
-  <cv-tabs-button
-    id="tabs-button-2"
-    content-selector="#tabs-panel-2"
-    tabs-panel-id="tabs-panel-2"
-  >
-    Tab label 2
-  </cv-tabs-button>
-  <cv-tabs-button
-    id="tabs-button-3"
-    content-selector="#tabs-panel-3"
-    tabs-panel-id="tabs-panel-3"
-  >
-    Tab label 3
-  </cv-tabs-button>
-  <cv-tabs-button
-    id="tabs-button-4"
-    content-selector="#tabs-panel-4"
-    tabs-panel-id="tabs-panel-4"
-  >
-    Tab label 4
-  </cv-tabs-button>
-  <cv-tabs-button
-    id="tabs-button-5"
-    content-selector="#tabs-panel-5"
-    tabs-panel-id="tabs-panel-5"
-  >
-    Tab label 5
-  </cv-tabs-button>
+<cv-tabs>
+  <cv-tab id="tabs-1" label="Tab link 1"> Sample tab panel content 1 </cv-tab>
+  <cv-tab id="tabs-2" label="Tab link 2"> Sample tab panel content 2 </cv-tab>
+  <cv-tab id="tabs-3" label="Tab link 3"> Sample tab panel content 3 </cv-tab>
+  <cv-tab id="tabs-4" label="Tab link 4"> Sample tab panel content 4 </cv-tab>
+  <cv-tab id="tabs-5" label="Tab link 5"> Sample tab panel content 5 </cv-tab>
 </cv-tabs>
-
-<section>
-  <cv-tabs-panel id="tabs-panel-1" tabs-button-id="tabs-button-1" selected>
-    Sample tab panel content 1
-  </cv-tabs-panel>
-  <cv-tabs-panel id="tabs-panel-2" tabs-button-id="tabs-button-2">
-    Sample tab panel content 2
-  </cv-tabs-panel>
-  <cv-tabs-panel id="tabs-panel-3" tabs-button-id="tabs-button-3">
-    Sample tab panel content 3
-  </cv-tabs-panel>
-  <cv-tabs-panel id="tabs-panel-4" tabs-button-id="tabs-button-4">
-    Sample tab panel content 4
-  </cv-tabs-panel>
-  <cv-tabs-panel id="tabs-panel-5" tabs-button-id="tabs-button-5">
-    Sample tab panel content 5
-  </cv-tabs-panel>
-</section>
 ```
+
+## CvTabs
+
+### Events
+
+- tab-selected
 
 ## CvTab
 
 ### Slots
 
-- default: Location for CvTabButtons
+- default: Location for tab content
 
-### Events
+### Attributes - cv-tab
 
-- tab-selected
-- tab-beingselected : cancellable
-
-## CvTabButton
-
-### Attributes
-
-- id: used for aria controlled by
-- content-selector: tab panel associated with CvTabButton
-- selected: tab button initially selected.
-- tabs-panel-id: Used for aria controls
-
-### Slots
-
-- default: content of button
-
-## CvTabPanel
-
-### Attributes
-
-- id: used for aria controls
-- selected: visible when first displayed
-- tabs-panel-id: used for aria controlled by
+- id: required
+- selected: optional
+- label: required label for tab link
--- src/components/cv-tabs/cv-tabs.vue
+++ src/components/cv-tabs/cv-tabs.vue
@@ -1,31 +1,121 @@
 <template>
-  <nav data-tabs class="cv-tabs bx--tabs" role="navigation" v-on="$listeners">
-    <div class="bx--tabs-trigger" tabindex="0">
-      <a
-        href="javascript:void(0)"
-        class="bx--tabs-trigger-text"
-        tabindex="-1"
-      ></a>
-      <svg width="10" height="5" viewBox="0 0 10 5">
-        <path d="M0 0l5 4.998L10 0z" fill-rule="evenodd"></path>
-      </svg>
-    </div>
-    <ul class="bx--tabs__nav bx--tabs__nav--hidden" role="tablist">
-      <slot> <!-- cv-tabs-button's go here --> </slot>
-    </ul>
-  </nav>
+  <div class="cv-tabs">
+    <nav
+      data-tabs
+      class="cv-tab bx--tabs"
+      role="navigation"
+      v-on="$listeners"
+      @keydown.right.prevent="moveRight"
+      @keydown.left.prevent="moveLeft"
+    >
+      <div class="bx--tabs-trigger" tabindex="0">
+        <a
+          href="javascript:void(0)"
+          class="bx--tabs-trigger-text"
+          tabindex="-1"
+        ></a>
+        <svg width="10" height="5" viewBox="0 0 10 5">
+          <path d="M0 0l5 4.998L10 0z" fill-rule="evenodd"></path>
+        </svg>
+      </div>
+      <ul class="bx--tabs__nav bx--tabs__nav--hidden" role="tablist">
+        <li
+          v-for="(tab, index) in tabs"
+          :key="index"
+          class="cv-tabs-button bx--tabs__nav-item"
+          :class="{
+            'bx--tabs__nav-item--selected': selectedIndex == index,
+          }"
+          role="presentation"
+        >
+          <a
+            class="bx--tabs__nav-link"
+            href="javascript:void(0)"
+            role="tab"
+            :aria-controls="tab.id"
+            :id="`${tab.id}-link`"
+            aria-selected="false"
+            @click="onTabClick(index)"
+            ref="link"
+            >{{ tab.label }}</a
+          >
+        </li>
+      </ul>
+    </nav>
+    <div class="cv-tabs__panels"><slot></slot></div>
+  </div>
 </template>
 
 <script>
-import { Tab } from 'carbon-components';
-
 export default {
   name: 'CvTabs',
-  mounted() {
-    this.carbonComponent = Tab.create(this.$el);
+  data() {
+    return {
+      tabs: [],
+      selectedIndex: 0,
+    };
   },
-  beforeDestroy() {
-    this.carbonComponent.release();
+  methods: {
+    register(tab) {
+      this.tabs.push(tab);
+      this.checkSelected();
+    },
+    deregister(tab) {
+      const tabIndex = this.tabs.findIndex(item => item.id === tab.id);
+      if (tabIndex > -1) {
+        this.tabs.splice(tabIndex, 1);
+      }
+      this.checkSelected();
+    },
+    onTabClick(index) {
+      this.selectedIndex = index;
+      for (let i = 0; i < this.tabs.length; i++) {
+        this.tabs[i].internalSelected = i === index;
+      }
+      this.$emit('tab-selected', index);
+    },
+    selectById(id) {
+      for (let i = 0; i < this.tabs.length; i++) {
+        if (this.tabs[i].id === id) {
+          this.onTabClick(i);
+          continue;
+        }
+      }
+    },
+    checkSelected() {
+      let somethingSelected = false;
+      if (this.tabs.length) {
+        for (let i = 0; i < this.tabs.length; i++) {
+          if (this.tabs[i].internalSelected) {
+            this.onTabClick(i);
+            somethingSelected = true;
+          }
+        }
+        if (!somethingSelected) {
+          this.onTabClick(0);
+        }
+      }
+    },
+    moveLeft() {
+      let newIndex;
+      if (this.selectedIndex > 0) {
+        newIndex = this.selectedIndex - 1;
+      } else {
+        newIndex = this.tabs.length - 1;
+      }
+      this.onTabClick(newIndex);
+      this.$refs.link[newIndex].focus();
+    },
+    moveRight() {
+      let newIndex;
+      if (this.selectedIndex < this.tabs.length - 1) {
+        newIndex = this.selectedIndex + 1;
+      } else {
+        newIndex = 0;
+      }
+      this.onTabClick(newIndex);
+      this.$refs.link[newIndex].focus();
+    },
   },
 };
 </script>
