a36a9cfb7c95929f90635dea2194a270df798293
Lee Chase <lee.chase@uk.ibm.com>
2019-01-23 11:29:40 +0000

    feat: add router link

--- src/components/cv-link/cv-link.vue
+++ src/components/cv-link/cv-link.vue
@@ -1,10 +1,18 @@
 <template>
-  <a class="cv-link bx--link" v-on="$listeners"> <slot></slot> </a>
+  <component :is="tagType" class="cv-link bx--link" v-on="$listeners">
+    <slot></slot>
+  </component>
 </template>
 
 <script>
 export default {
   name: 'CvLink',
+  computed: {
+    tagType() {
+      // if to is used and not href then user wanted a router-link
+      return this.$attrs.to && !this.$attrs.href ? 'router-link' : 'a';
+    },
+  },
   beforeCreate() {
     console.warn(
       `${this.$options._componentTag}: review use/offer a router-link option? `
