5dcfb4065805384ab4479bc122980220377ed6c2
Lee Chase <lee.chase@uk.ibm.com>
2019-01-25 14:10:49 +0000

    chore: merge

---
+++ src/index-components-vue.js
@@ -0,0 +1,9 @@
+console.error(
+  'The built component files have been renamed from "carbon-components-vue..." to "carbon-vue..."'
+);
+
+alert(
+  `The built component files have been renamed from "carbon-components-vue..." to "carbon-vue..."
+
+  See the notes in the release v0_3_0.`
+);
