46930aab480d77d7e1f710923db79ae1a2b8a6e3
Lee Chase <leechase@live.com>
2019-01-31 14:19:08 +0000

    chore: remove register n deregister from content switcher (#153)
    * chore: remove register n deregister from content switcher
    
    * chore: remove dead code

--- src/components/cv-content-switcher/cv-content-switcher.vue
+++ src/components/cv-content-switcher/cv-content-switcher.vue
@@ -5,39 +5,41 @@
 </template>
 
 <script>
+const toggleContent = (selector, on) => {
+  // hide content
+  const content = document.querySelectorAll(selector);
+  for (const element of content) {
+    // element.style.visibility = on;
+    if (!on) {
+      element.setAttribute('hidden', 'hidden');
+    } else {
+      element.removeAttribute('hidden');
+    }
+    element.setAttribute('aria-hidden', `${!on}`);
+  }
+};
+
 export default {
   name: 'CvContentSwitcher',
-  data() {
-    return {
-      switcherButtons: [],
-    };
+  created() {
+    // add these on created otherwise cv:mounted is too early.
+    this.$on('cv:open', srcComponent => this.onCvOpen(srcComponent));
+    this.$on('cv:mounted', srcComponent => this.onCvMount(srcComponent));
   },
   methods: {
-    onOpen(buttonId) {
-      let openButton;
+    onCvMount(srcComponent) {
+      toggleContent(srcComponent.contentSelector, srcComponent.isSelected);
+    },
+    onCvOpen(srcComponent) {
+      this.$emit('selected', srcComponent.contentSelector);
+      toggleContent(srcComponent.contentSelector, true);
 
-      for (let index in this.switcherButtons) {
-        if (this.switcherButtons[index].buttonId !== buttonId) {
-          this.switcherButtons[index].closeFunction();
-        } else {
-          openButton = this.switcherButtons[index];
+      for (let index in this.$children) {
+        if (this.$children[index].buttonId !== srcComponent.buttonId) {
+          this.$children[index].close();
+          toggleContent(this.$children[index].contentSelector, false);
         }
       }
-      if (openButton !== undefined) {
-        this.$emit('selected', openButton.contentSelector);
-      }
-    },
-    register(buttonId, contentSelector, closeFunction) {
-      this.deregister(buttonId);
-      this.switcherButtons.push({ buttonId, contentSelector, closeFunction });
-
-      return this.onOpen;
-    },
-    deregister(buttonId) {
-      let index = this.switcherButtons.findIndex(
-        item => item.buttonId === buttonId
-      );
-      this.switcherButtons.slice(index, index + 1);
     },
   },
 };
