d793f99210504e07d549f588069da26b882a5225
Lee Chase <lee.chase@uk.ibm.com>
2019-01-31 15:15:40 +0000

    fix: make uid more unique

--- src/mixins/uid-mixin.js
+++ src/mixins/uid-mixin.js
@@ -1,8 +1,12 @@
 const idGen = {
-  lastId: 0,
   newId() {
-    this.lastId++;
-    return `uid-${this.lastId}`;
+    // as it is funny gen IDs using b - https://gist.github.com/jed/982883
+    const b = a =>
+      a
+        ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+        : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b);
+
+    return `uid-${b()}`;
   },
 };
 
