a9e8c3aff42d0d01b74c0ec8153fdb5f6683e1c5
Lee Chase <lee.chase@uk.ibm.com>
2019-02-03 21:58:32 +0000

    chore: scratch head

--- tests/unit/cv-tag.spec.js
+++ tests/unit/cv-tag.spec.js
@@ -7,14 +7,16 @@ describe('CvTag.vue', () => {
     const wrapper = shallowMount(CvTag, {
       propsData: { label },
     });
-    // console.log(wrapper.html());
+
     expect(wrapper.props('label')).toBe(label);
     expect(wrapper.text()).toMatch(label);
   });
-});
 
-describe('CvTag.vue', () => {
   it('checks props.label is required', () => {
     expect(CvTag.props.label.required).toBe(true);
+  });
+
+  it('checks props.kind has default', () => {
+    expect(CvTag.props.kind.default).toBeDefined();
   });
 });
