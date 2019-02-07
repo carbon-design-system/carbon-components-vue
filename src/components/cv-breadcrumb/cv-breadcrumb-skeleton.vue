57d160a587a421136b0e72ab542693178df2cb54
Oleksandr Sabov <sabov@de.ibm.com>
2019-02-07 15:03:11 +0100

    feat: add BreadcrumbSkeleton component

---
+++ src/components/cv-breadcrumb/cv-breadcrumb-skeleton.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="bx--breadcrumb bx--skeleton" aria-label="breadcrumb">
+    <cv-breadcrumb-item-skeleton></cv-breadcrumb-item-skeleton>
+    <cv-breadcrumb-item-skeleton></cv-breadcrumb-item-skeleton>
+    <cv-breadcrumb-item-skeleton></cv-breadcrumb-item-skeleton>
+  </nav>
+</template>
+
+<script>
+import CvBreadcrumbItemSkeleton from './_cv-breadcrumb-item-skeleton';
+
+export default {
+  name: 'CvBreadcrumbSkeleton',
+  components: {
+    CvBreadcrumbItemSkeleton,
+  },
+};
+</script>
+
+<style lang="scss"></style>
