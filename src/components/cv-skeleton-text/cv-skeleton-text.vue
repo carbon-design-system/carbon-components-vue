8bf2badf2afc27415e64cf0c43e15d0959fa5d2e
Oleksandr Sabov <sabov@de.ibm.com>
2019-02-11 17:32:23 +0100

    feat: update CvSkeletonText component

--- src/components/cv-skeleton-text/cv-skeleton-text.vue
+++ src/components/cv-skeleton-text/cv-skeleton-text.vue
@@ -1,9 +1,13 @@
 <template>
-  <p
-    class="bx--skeleton__text"
-    :class="{ 'bx--skeleton__heading': heading }"
-    :style="{ width: width }"
-  ></p>
+  <div>
+    <p
+      class="bx--skeleton__text"
+      :class="{ 'bx--skeleton__heading': heading }"
+      :style="{ width: line.width }"
+      :key="index"
+      v-for="(line, index) in lines"
+    ></p>
+  </div>
 </template>
 
 <script>
@@ -12,6 +16,48 @@ export default {
   props: {
     width: { type: String, default: '100%' },
     heading: { type: Boolean, default: false },
+    lineCount: { type: Number, default: 3 },
+    paragraph: { type: Boolean, default: false },
+  },
+  methods: {
+    getRandomInt: function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    calcWidth: function() {
+      let width = this.width;
+      if (this.paragraph) {
+        const { num, unit } = this.widthObj;
+        if (unit === '%') {
+          const randomWidth = this.getRandomInt(0, 75) + 'px';
+          width = `calc(${width} - ${randomWidth})`;
+        } else if (unit === 'px') {
+          width = this.getRandomInt(num - 75, num) + 'px';
+        }
+      }
+      return width;
+    },
+  },
+  computed: {
+    widthObj: function() {
+      const widthObj = { num: parseInt(this.width, 10) };
+      if (this.width.includes('px')) {
+        widthObj.unit = 'px';
+      }
+      if (this.width.includes('%')) {
+        widthObj.unit = '%';
+      }
+      return widthObj;
+    },
+    lines: function() {
+      return Array.from(
+        {
+          length: this.paragraph ? this.lineCount : 1,
+        },
+        (el, i) => ({
+          width: this.calcWidth(),
+        })
+      );
+    },
   },
 };
 </script>
