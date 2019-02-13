6310e55a7304fb60a034157b6941fc0b5ef241cf
Lee Chase <leechase@live.com>
2019-02-13 22:17:51 +0000

    feat: add table actions (#207)

---
+++ src/components/cv-data-table/cv-data-table-action.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="bx--toolbar-action" v-on="$listeners">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'cv-data-table-action',
+  mounted() {
+    // ensure SVG has
+    this.$el.children[0].classList.add('class="bx--toolbar-action__icon');
+  },
+};
+</script>
