9684344ee7c139d3832a223cdef822fdbd19131f
Lee Chase <lee.chase@uk.ibm.com>
2019-02-15 15:52:39 +0000

    chore: in progress

---
+++ src/components/cv-data-table/cv-data-gable-row.vue
@@ -0,0 +1,38 @@
+<template>
+  <tr>
+    <td v-if="hasBatchActions">
+      <cv-checkbox
+        :form-item="false"
+        :value="`${key}`"
+        v-model="rowChecks"
+        @change="onRowCheckChange"
+        ref="checkbox"
+      />
+    </td>
+    <td
+      v-for="(cell, colIndex) in data"
+      :key="`cell:${colIndex}:${key}`"
+      :style="dataStyle(colIndex)"
+    >
+      {{ cell }}
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'CvDataTableRow',
+  props: {
+    key: { type: String, required: true },
+    data: Array,
+  },
+  computed: {
+    hasBatchActions() {
+      return this.$parent.hasBatchActions;
+    },
+    isChecked() {
+      return this.$refs.isChecked.isChecked;
+    },
+  },
+};
+</script>
