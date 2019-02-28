63c6e639e2ef323c2c6215d86c9503f8706321e4
Conrad Schmidt <conrad_schmidt@gmx.de>
2019-02-28 11:21:53 +0100

    feat: add CvFileUploadSkeleton component (#228)
    * feat: add CvFileUploadSkeleton component
    
    * feat: fix linting errors

---
+++ src/components/cv-file-uploader/cv-file-uploader-skeleton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bx--form-item">
+    <cv-skeleton-text heading width="100px" />
+    <cv-skeleton-text width="225px" class="bx--label-descriptio" />
+    <cv-button-skeleton />
+  </div>
+</template>
+
+<script>
+import CvSkeletonText from '../cv-skeleton-text/cv-skeleton-text';
+import CvButtonSkeleton from '../cv-button/cv-button-skeleton';
+
+export default {
+  name: 'CvFileUploaderSkeleton',
+  components: {
+    CvSkeletonText,
+    CvButtonSkeleton,
+  },
+};
+</script>
+
+<style lang="scss"></style>
