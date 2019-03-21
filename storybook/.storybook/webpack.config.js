d1dd9bdd7e45f77a4cc6b38c85832397f0e1e253
Lee Chase <lee.chase@uk.ibm.com>
2019-03-21 18:53:30 +0000

    feat: storybook 5

--- storybook/.storybook/webpack.config.js
+++ storybook/.storybook/webpack.config.js
@@ -1,34 +1,34 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer')({ browsers: ['last 2 versions'] });
+const autoprefixer = require('autoprefixer');
 
-module.exports = (storybookBaseConfig, configType, defaultConfig) => {
-  defaultConfig.module.rules.push({
-    test: /-story\.js$/,
-    include: path.resolve(__dirname, '../src'),
-    loader: require.resolve('@storybook/addon-storysource/loader'),
-    options: {
-      prettierConfig: {
-        parser: 'babel', // Remove warnings when loading story source files
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: [/-story\.js$/],
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        include: [path.resolve(__dirname, '../src')],
+        enforce: 'pre',
       },
-    },
-    enforce: 'pre',
-  });
-
-  defaultConfig.module.rules.push({
-    test: /\.(s){0,1}css$/,
-    loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-    include: path.resolve(__dirname, '../../'),
-  });
-
-  // auto prefix anything in a vue file
-  defaultConfig.resolve.extensions.push('.js', '.vue', '.json');
-  let vueLoaderConfig = defaultConfig.module.rules.find(item => {
-    return item.loader && item.loader.indexOf('vue-loader') > -1;
-  });
-  vueLoaderConfig.options = {
-    ...vueLoaderConfig.options,
-    postcss: [autoprefixer],
-  };
-
-  return defaultConfig;
+      {
+        test: /\.((s){0,1}css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer({ browsers: ['last 2 versions'] })],
+            },
+          },
+          'sass-loader',
+        ],
+        include: path.resolve(__dirname, '../'),
+      },
+    ],
+  },
 };
