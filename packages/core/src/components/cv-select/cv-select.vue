27541dbe1468a0a1046f510085e182a5bd9ce2a6
Lee Chase <leechase@live.com>
2019-03-26 15:41:56 +0000

    feat: select carbon x (#288)
    * chore: first steps
    
    * feat: add wrapper
    
    * feat: select carbon x

--- packages/core/src/components/cv-select/cv-select.vue
+++ packages/core/src/components/cv-select/cv-select.vue
@@ -1,24 +1,127 @@
 <template>
-  <component :is="tagType" v-bind="$attrs" v-on="$listeners" :value="value" class="cv-select">
-    <slot></slot>
-  </component>
+  <cv-wrapper :tag-type="formItem ? 'div' : ''" class="cv-select bx--form-item">
+    <div
+      class="bx--select"
+      :class="{
+        'cv-select': !formItem,
+        'bx--select--inline': inline,
+        'bx--select--light': theme === 'light',
+        'bx--select--invalid': isInvalid,
+      }"
+    >
+      <label :for="uid" class="bx--label" :class="{ 'bx--visually-hidden': hideLabel }">{{ label }}</label>
+
+      <div v-if="!inline && isHelper" class="bx--form__helper-text">
+        <slot name="helper-text">{{ helperText }}</slot>
+      </div>
+
+      <cv-wrapper :tag-type="inline && componentsX ? 'div' : ''" class="bx--select-input--inline__wrapper">
+        <div v-if="componentsX" class="bx--select-input__wrapper" :data-invalid="isInvalid">
+          <select v-bind="$attrs" :id="uid" class="bx--select-input" v-on="inputListeners" ref="select" :value="value">
+            <slot></slot>
+          </select>
+          <chevron-down-glyph v-if="componentsX" class="bx--select__arrow" />
+          <warning-filled-16 v-if="componentsX && isInvalid" class="bx--select__invalid-icon" />
+        </div>
+
+        <div v-if="isInvalid && componentsX && isInvalid" class="bx--form-requirement">
+          <slot name="invalid-message">{{ invalidMessage }}</slot>
+        </div>
+        <!-- cv-wrapper div bx--select-input--inline__wrapper -->
+      </cv-wrapper>
+
+      <!-- NOTE componentsX -->
+      <select
+        v-if="!componentsX"
+        v-bind="$attrs"
+        :id="uid"
+        class="bx--select-input"
+        v-on="inputListeners"
+        ref="select"
+        :value="value"
+        :data-invalid="isInvalid"
+      >
+        <slot></slot>
+      </select>
+      <svg v-if="!componentsX" class="bx--select__arrow" width="10" height="5" viewBox="0 0 10 5">
+        <path d="M0 0l5 4.998L10 0z" fill-rule="evenodd"></path>
+      </svg>
+
+      <div v-if="inline && isHelper" class="bx--form__helper-text">
+        <slot name="helper-text">{{ helperText }}</slot>
+      </div>
+
+      <div v-if="!componentsX && isInvalid" class="bx--form-requirement">
+        <slot name="invalid-message">{{ invalidMessage }}</slot>
+      </div>
+    </div>
+    <!-- cv-wrapper div bx--form-item -->
+  </cv-wrapper>
 </template>
 
 <script>
-import CvSelectInnerFormItem from './_cv-select-inner-form-item';
-import CvSelectInner from './_cv-select-inner';
+import uidMixin from '../../mixins/uid-mixin';
+import themeMixin from '../../mixins/theme-mixin';
+import { componentsX } from '../../_internal/_feature-flags';
+import CvWrapper from '../cv-wrapper/_cv-wrapper';
+import ChevronDownGlyph from '@carbon/icons-vue/lib/chevron--down/index';
+import WarningFilled16 from '@carbon/icons-vue/lib/warning--filled/16';
 
 export default {
   name: 'CvSelect',
-  components: { CvSelectInnerFormItem, CvSelectInner },
   inheritAttrs: false,
+  components: { CvWrapper, ChevronDownGlyph, WarningFilled16 },
+  mixins: [uidMixin, themeMixin],
   props: {
-    value: String,
-    formItem: { type: Boolean, default: true },
+    inline: Boolean,
+    invalidMessage: { type: String, default: null },
+    helperText: { type: String, default: null },
+    formItem: { type: Boolean, default: false },
+    hideLabel: Boolean,
+    label: { type: String, required: true },
+    // *********************
+    // declare here to prevent the following from being added to the select
+    // *********************
+    // multiple does not work with styling from carbon-components 9.20
+    multiple: {
+      type: String,
+      validator: () => {
+        console.warn('property multiple not supported in CvSelect');
+        return false;
+      },
+    },
+    value: { type: String },
+  },
+  beforeCreate() {
+    // *********************
+    // delete here to prevent the following from being added to the select
+    // *********************
+    // multiple does not work with styling from carbon-components 9.20
+    delete this.$attrs.multiple;
+  },
+  data() {
+    return { componentsX };
   },
   computed: {
-    tagType() {
-      return this.formItem ? 'cv-select-inner-form-item' : 'cv-select-inner';
+    // Bind listeners at the component level to the embedded input element and
+    // add our own input listener to service the v-model. See:
+    // https://vuejs.org/v2/guide/components-custom-events.html#Customizing-Component-v-model
+    inputListeners() {
+      return {
+        ...this.$listeners,
+        input: event => this.$emit('input', event.target.value),
+      };
+    },
+    isInvalid() {
+      return this.$slots['invalid-message'] !== undefined || (this.invalidMessage && this.invalidMessage.length);
+    },
+    isHelper() {
+      return this.$slots['helper-text'] !== undefined || (this.helperText && this.helperText.length);
+    },
+  },
+  methods: {
+    test() {
+      alert('hi');
     },
   },
 };
