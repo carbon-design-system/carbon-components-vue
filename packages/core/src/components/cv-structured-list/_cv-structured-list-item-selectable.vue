30f29bbdf70139ce722034cb581c8b702b73061e
Lee Chase <lee.chase@uk.ibm.com>
2019-03-29 10:51:56 +0000

    feat: add structured list carbon x

--- packages/core/src/components/cv-structured-list/_cv-structured-list-item-selectable.vue
+++ packages/core/src/components/cv-structured-list/_cv-structured-list-item-selectable.vue
@@ -7,6 +7,7 @@
     tabindex="0"
   >
     <input
+      v-if="!componentsX"
       v-bind="$attrs"
       v-on="inputListeners"
       tabindex="-1"
@@ -17,7 +18,8 @@
       type="radio"
     />
     <div class="bx--structured-list-td">
-      <svg class="bx--structured-list-svg" width="16" height="16" viewBox="0 0 16 16">
+      <CheckmarkFilled16 v-if="componentsX" class="bx--structured-list-svg" />
+      <svg v-else class="bx--structured-list-svg" width="16" height="16" viewBox="0 0 16 16">
         <path
           d="M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16zm3.646-10.854L6.75 10.043 4.354 7.646l-.708.708 3.104 3.103 5.604-5.603-.708-.708z"
           fill-rule="evenodd"
@@ -25,17 +27,30 @@
       </svg>
     </div>
     <slot></slot>
+    <input
+      v-if="componentsX"
+      v-bind="$attrs"
+      v-on="inputListeners"
+      tabindex="-1"
+      :id="uid"
+      class="bx--structured-list-input"
+      :checked="isChecked"
+      :value="value"
+      type="radio"
+    />
   </label>
 </template>
 
 <script>
 import uidMixin from '../../mixins/uid-mixin';
 import radioMixin from '../../mixins/radio-mixin';
+import componentsX from '../../internal/feature-flags';
 
 export default {
   name: 'CvStructuredListItemSelectable',
   inheritAttrs: false,
   mixins: [uidMixin, radioMixin],
+  data: () => ({ componentsX }),
 };
 </script>
 
