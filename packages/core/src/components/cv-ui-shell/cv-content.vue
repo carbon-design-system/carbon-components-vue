de53b3fa8ea473577d35ae4631fbb5469785e746
Lee Chase <leechase@live.com>
2019-07-09 16:43:53 +0100

    feat: add CvContent (#473)

---
+++ packages/core/src/components/cv-ui-shell/cv-content.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="tagType" class="bx--content">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'CvContent',
+  props: {
+    tagType: { type: String, default: 'main' },
+  },
+};
+</script>
+
+<style lang="scss"></style>
