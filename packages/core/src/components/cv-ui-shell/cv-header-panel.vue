44abd0fb29e7b9e6485704e0c8a4127bad21338e
Lee Chase <leechase@live.com>
2019-09-05 13:21:15 +0100

    chore: adjust header panel events (#527)

--- packages/core/src/components/cv-ui-shell/cv-header-panel.vue
+++ packages/core/src/components/cv-ui-shell/cv-header-panel.vue
@@ -43,9 +43,11 @@ export default {
         return this.dataExpanded;
       },
       set(val) {
-        this.dataExpanded = val;
-        this.$emit('panel-resize', this);
-        this.$emit('modelEvent', val);
+        if (this.dataExpanded !== val) {
+          this.dataExpanded = val;
+          this.$emit('modelEvent', val);
+          this.$emit('panel-resize', this);
+        }
       },
     },
   },
