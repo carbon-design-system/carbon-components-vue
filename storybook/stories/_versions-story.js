127a7fce76e2f7c0305dc2b3d39b5b0d7125f158
Dave Clark <dave.clark@uk.ibm.com>
2019-11-26 17:50:38 +0000

    feat: switch storybook to dark theme

--- storybook/stories/_versions-story.js
+++ storybook/stories/_versions-story.js
@@ -3,12 +3,21 @@ import SvVersions from '../_storybook/views/sv-template-view/sv-versions';
 
 const stories = storiesOf('Versions', module);
 
-stories.add('default', () => {
-  // ----------------------------------------------------------------
-  const templateViewString = `<sv-versions></sv-versions>`;
+stories.add(
+  'default',
+  () => {
+    // ----------------------------------------------------------------
+    const templateViewString = `<sv-versions></sv-versions>`;
 
-  return {
-    components: { SvVersions },
-    template: templateViewString,
-  };
-});
+    return {
+      components: { SvVersions },
+      template: templateViewString,
+    };
+  },
+  {
+    options: {
+      isToolshown: false,
+      showPanel: false,
+    },
+  }
+);
