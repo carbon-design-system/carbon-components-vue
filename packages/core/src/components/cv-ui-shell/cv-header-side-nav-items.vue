6de53fb83bb01aa26b19d0527f87203f68d5c304
Lee Chase <leechase@live.com>
2019-12-11 01:38:42 -0800

    feat: add rail reature to side nav (#744)
    * feat: add rail reature to side nav
    
    * chore: update notes

---
+++ packages/core/src/components/cv-ui-shell/cv-header-side-nav-items.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bx--side-nav__header-navigation" :class="{ 'bx--side-nav__header-divider': divider }"><slot /></div>
+</template>
+
+<script>
+export default {
+  name: 'CvHeaderSideNavItems',
+  props: {
+    divider: Boolean,
+  },
+};
+</script>
