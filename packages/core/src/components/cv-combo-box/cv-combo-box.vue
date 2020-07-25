2c2c1969fe0d0454e04115f1fa6186bc8d5483fd
Lee Chase <lee.chase@uk.ibm.com>
2020-07-25 22:15:44 +0100

    fix: combo and multi empty class

--- packages/core/src/components/cv-combo-box/cv-combo-box.vue
+++ packages/core/src/components/cv-combo-box/cv-combo-box.vue
@@ -44,7 +44,10 @@
       >
         <input
           ref="input"
-          :class="[`${carbonPrefix}--text-input`, { [`${carbonPrefix}--text-input--empty`]: dataInput.length === 0 }]"
+          :class="[
+            `${carbonPrefix}--text-input`,
+            { [`${carbonPrefix}--text-input--empty`]: dataInput && dataInput.length === 0 },
+          ]"
           :aria-controls="uid"
           aria-autocomplete="list"
           role="combobox"
