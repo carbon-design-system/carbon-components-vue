2cac99cdbff6f60ca44a678880bfd077b0a91df0
Lee Chase <lee.chase@uk.ibm.com>
2020-07-30 17:01:14 +0100

    fix: ie11 svg class usage

--- packages/core/src/components/cv-data-table/cv-data-table-action.vue
+++ packages/core/src/components/cv-data-table/cv-data-table-action.vue
@@ -13,9 +13,12 @@ export default {
     // ensure SVG has
     if (this.$el.children[0].classList) {
       this.$el.children[0].classList.add(`${this.carbonPrefix}--toolbar-action__icon`);
-    } else if (this.$el.children[0].className) {
+    } else if (this.$el.children[0].class) {
       // IE 11 and Edge pre-crhomium
-      this.$el.children[0].className += ` ${this.carbonPrefix}--toolbar-action__icon`;
+      this.$el.children[0].setAttribute(
+        'class',
+        `${this.$el.children[0].getAttribute('class')} ${this.carbonPrefix}--toolbar-action__icon`
+      );
     }
   },
 };
