462b3dcc849810ca9fe92d846c21101037a8c43a
David Nixon <david_nixon@us.ibm.com>
2020-12-17 16:47:09 -0500

    fix: added storybook - used cv-toggle as the pattern

--- storybook/stories/cv-tooltip-story.js
+++ storybook/stories/cv-tooltip-story.js
@@ -1,5 +1,6 @@
 import { storiesOf } from '@storybook/vue';
 import { text, select, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 import SvTemplateView from '../_storybook/views/sv-template-view/sv-template-view';
 // import consts from '../_storybook/utils/consts';
@@ -75,9 +76,18 @@ let preKnobs = {
     config: ['visible', false], // consts.CONFIG], // fails when used with number in storybook 4.1.4
     prop: 'visible',
   },
+  events: {
+    group: 'attr',
+    value: `v-on:show-tooltip="actionTooltipShow"
+  v-on:hide-tooltip="actionTooltipHide"`,
+  },
 };
 
-const variants = [{ name: 'default' }, { name: 'minimal', includes: ['content', 'definition', 'term', 'tip'] }];
+const variants = [
+  { name: 'default', excludes: ['events'] },
+  { name: 'minimal', includes: ['content', 'definition', 'term', 'tip'] },
+  { name: 'events', includes: ['content', 'visible', 'events'] },
+];
 
 let storySet = knobsHelper.getStorySet(variants, preKnobs);
 
@@ -114,6 +124,8 @@ for (const story of storySet) {
         template: templateViewString,
         props: settings.props,
         methods: {
+          actionTooltipShow: action('CV Tooltip - show'),
+          actionTooltipHide: action('CV Tooltip - hide'),
           doStart() {
             this.$refs.templateView.$slots.component[0].componentInstance.show();
           },
