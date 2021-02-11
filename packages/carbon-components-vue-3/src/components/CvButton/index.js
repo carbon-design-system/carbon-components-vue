42387b9fee88319daddc8a1fe1048843459b98d6
Lee Chase <lee.chase@uk.ibm.com>
2021-02-11 12:13:04 +0000

    chore: remove unused folder

--- packages/carbon-components-vue-3/src/components/CvButton/index.js
+++
@@ -1,6 +0,0 @@
-import CvButton from './CvButton';
-import CvIconButton from './CvIconButton';
-import CvButtonConsts from './consts';
-
-export { CvButton, CvButtonConsts, CvIconButton };
-export default CvButton;
