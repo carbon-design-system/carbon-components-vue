2805cbbdd918506dc53e76bc946cae35c2c3faf2
Lee Chase <leechase@live.com>
2021-02-21 13:42:44 +0000

    feat: v3 improve story source for CvButton (#1128)
    * feat: v3 improve story source for CvButton
    
    * chore: storybook package bump
    
    Co-authored-by: Lee Chase <lee.chase@uk.ibm.com>

--- packages/v3/src/components/CvButton/CvButton.stories.js
+++ packages/v3/src/components/CvButton/CvButton.stories.js
@@ -1,9 +1,10 @@
-import { shallowRef } from '@vue/reactivity';
-
+import { action } from '@storybook/addon-actions';
 import { CvButton } from './';
 import { buttonKinds, buttonSizes } from './consts.js';
-import { storybookControlsFromProps } from '../../global/storybook-utils';
-
+import {
+  storybookControlsFromProps,
+  storyParametersObject,
+} from '../../global/storybook-utils';
 import { props as commonCvButtonProps } from './CvButtonCommon';
 import {
   Bee20,
@@ -32,62 +33,77 @@ export default {
     kind: { control: { type: 'select', options: buttonKinds } },
     size: { control: { type: 'select', options: buttonSizes } },
   },
-  parameters: {
-    actions: {
-      handles: ['mouseover', 'mousedown .bx--btn', 'click'],
-    },
-  },
 };
 
+const template = `<cv-button @click="onClick" v-bind="newArgs">{{defaultSlot}}</cv-button>`;
 const Template = (args, { argTypes }) => {
+  const newArgs = { ...args, icon: icons[args.icon] };
   return {
     props: Object.keys(argTypes),
     components: { CvButton },
-    template: `<cv-button :data-test="label" v-bind="$props" :icon="icon">{{label}}</cv-button>`,
-    setup(props) {
-      const icon = shallowRef(icons[props.icon]);
-
-      if (icon.value === undefined) {
-        // assigning an icon seem sto need a kick
-        setTimeout(() => {
-          console.log(props.icon);
-          icon.value === icons[props.icon];
-          console.dir(icon.value);
-        }, 1);
-      }
-
-      return { icon };
+    setup() {
+      return { newArgs, onClick: action('click') };
     },
+    template,
   };
 };
 
 export const Primary = Template.bind({});
 Primary.args = {
   kind: 'primary',
-  label: 'primary',
-  banana: 'banana',
+  defaultSlot: 'Primary',
 };
+Primary.parameters = storyParametersObject(
+  Primary.parameters,
+  template,
+  Primary.args,
+  'v-bind="newArgs"'
+);
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   kind: 'secondary',
-  label: 'Secondary',
+  defaultSlot: 'Secondary',
 };
+Secondary.parameters = storyParametersObject(
+  Secondary.parameters,
+  template,
+  Secondary.args,
+  'v-bind="newArgs"'
+);
 
 export const Field = Template.bind({});
 Field.args = {
-  label: 'Field size',
+  defaultSlot: 'Field size',
   size: 'field',
 };
+Field.parameters = storyParametersObject(
+  Field.parameters,
+  template,
+  Field.args,
+  'v-bind="newArgs"'
+);
 
 export const Small = Template.bind({});
 Small.args = {
-  label: 'sm',
+  defaultSlot: 'sm',
   size: 'sm',
 };
+Small.parameters = storyParametersObject(
+  Small.parameters,
+  template,
+  Small.args,
+  'v-bind="newArgs"'
+);
 
 export const Large = Template.bind({});
 Large.args = {
-  label: 'Large size',
-  size: 'large',
+  defaultSlot: 'Large size',
+  size: 'lg',
 };
+Large.parameters = storyParametersObject(
+  Large.parameters,
+  template,
+  Large.args,
+  'v-bind="newArgs"'
+);
--- packages/v3/src/components/CvButton/CvIconButton.stories.js
+++ packages/v3/src/components/CvButton/CvIconButton.stories.js
@@ -1,7 +1,10 @@
-import { computed } from 'vue';
+import { action } from '@storybook/addon-actions';
 import { CvIconButton } from './';
 import { buttonKinds, buttonSizes } from './consts.js';
-import { storybookControlsFromProps } from '../../global/storybook-utils';
+import {
+  storybookControlsFromProps,
+  storyParametersObject,
+} from '../../global/storybook-utils';
 
 import { props as commonCvButtonProps } from './CvButtonCommon';
 import {
@@ -45,21 +48,17 @@ export default {
      */
     size: { control: { type: 'select', options: buttonSizes } },
   },
-  parameters: {
-    actions: {
-      handles: ['mouseover', 'mousedown .bx--btn', 'click'],
-    },
-  },
 };
 
+const template = `<cv-icon-button @click="onClick" v-bind="newArgs" />`;
 const Template = (args, { argTypes }) => {
+  const newArgs = { ...args, icon: icons[args.icon] };
   return {
     props: Object.keys(argTypes),
     components: { CvIconButton },
-    template: `<cv-icon-button v-bind="$props" :icon="theIcon"/>`,
-    setup(props) {
-      const theIcon = computed(() => icons[props.icon]);
-      return { theIcon };
+    template,
+    setup() {
+      return { newArgs, onClick: action('click') };
     },
   };
 };
@@ -70,6 +69,12 @@ Primary.args = {
   label: 'primary',
   icon: 'Bee20',
 };
+Primary.parameters = storyParametersObject(
+  Primary.parameters,
+  template,
+  Primary.args,
+  'v-bind="newArgs"'
+);
 
 export const Secondary = Template.bind({});
 Secondary.args = {
@@ -77,6 +82,12 @@ Secondary.args = {
   label: 'Secondary',
   icon: 'Bee20',
 };
+Secondary.parameters = storyParametersObject(
+  Secondary.parameters,
+  template,
+  Secondary.args,
+  'v-bind="newArgs"'
+);
 
 export const Field = Template.bind({});
 Field.args = {
@@ -84,6 +95,12 @@ Field.args = {
   size: 'field',
   icon: 'Bee20',
 };
+Field.parameters = storyParametersObject(
+  Field.parameters,
+  template,
+  Field.args,
+  'v-bind="newArgs"'
+);
 
 export const Small = Template.bind({});
 Small.args = {
@@ -91,6 +108,12 @@ Small.args = {
   size: 'sm',
   icon: 'Bee20',
 };
+Small.parameters = storyParametersObject(
+  Small.parameters,
+  template,
+  Small.args,
+  'v-bind="newArgs"'
+);
 
 export const Large = Template.bind({});
 Large.args = {
@@ -98,3 +121,9 @@ Large.args = {
   size: 'lg',
   icon: 'Bee20',
 };
+Large.parameters = storyParametersObject(
+  Large.parameters,
+  template,
+  Large.args,
+  'v-bind="newArgs"'
+);
