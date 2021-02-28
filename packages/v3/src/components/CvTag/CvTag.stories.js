0eb65653d43737edf2523c2e9dfef4fa56b88bdb
Lee Chase <leechase@live.com>
2021-02-28 21:53:22 +0000

    Fontinalis v3/cv tag (#1135)
    * add an initial CvTag component to v3
    
    * update CvTag story
    
    * v3: update CvTag stories
    
    * v3: clean up CvTag component
    
    * add tests for full coverage on CvTag
    
    * feat: adds v3 tag
    
    * chore: update to lastest story syntax
    
    Co-authored-by: Fontinalis <barnabas.vac@gmail.com>
    Co-authored-by: Lee Chase <lee.chase@uk.ibm.com>

---
+++ packages/v3/src/components/CvTag/CvTag.stories.js
@@ -0,0 +1,65 @@
+import CvTag from './CvTag';
+import { tagKinds } from './consts.js';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'Components/CvTag',
+  component: CvTag,
+  argTypes: {
+    kind: { control: { type: 'select', options: tagKinds } },
+  },
+};
+
+const Template = (args, { argTypes }) => {
+  return {
+    props: Object.keys(argTypes),
+    components: { CvTag },
+    template: `<CvTag @remove="onRemove" v-bind="args" />`,
+    setup() {
+      return {
+        args,
+        onRemove: action('remove'),
+      };
+    },
+  };
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  kind: 'red',
+  label: 'This is a tag',
+  filter: false,
+};
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<CvTag
+  kind="red"
+  label="This is a tag"
+  :filter="false"
+/>
+`,
+    },
+  },
+};
+
+export const Filter = Template.bind({});
+Filter.args = {
+  kind: 'teal',
+  label: 'This is a tag',
+  filter: true,
+};
+Filter.parameters = {
+  docs: {
+    source: {
+      code: `
+<CvTag
+  kind="teal"
+  label="This is a tag"
+  :filter="true"
+/>
+`,
+    },
+  },
+};
