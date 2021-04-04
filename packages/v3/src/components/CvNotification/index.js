4273f8abe349cfcf4af16c7156a4817d88bec075
Timon Lukas <github@timonlukas.de>
2021-04-04 22:21:54 +0200

    Implement CvToastNotification component & story

--- packages/v3/src/components/CvNotification/index.js
+++ packages/v3/src/components/CvNotification/index.js
@@ -1,2 +1,3 @@
 export { default as CvInlineNotification } from './CvInlineNotification';
+export { default as CvToastNotification } from './CvToastNotification';
 export { default as CvNotificationConsts } from './consts';
