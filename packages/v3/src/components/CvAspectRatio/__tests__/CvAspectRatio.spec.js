eaf2af5e0da53b8990cd0949fab007e1e52cd397
Timon Lukas <github@timonlukas.de>
2021-04-04 20:01:34 +0200

    Add tests

---
+++ packages/v3/src/components/CvAspectRatio/__tests__/CvAspectRatio.spec.js
@@ -0,0 +1,42 @@
+import { shallowMount } from '@vue/test-utils';
+import { carbonPrefix } from '../../../global/settings';
+
+import { CvAspectRatio, CvAspectRatioConsts } from '..';
+
+describe('CvAspectRatio', () => {
+  it('CvAspectRatio - default', () => {
+    const wrapper = shallowMount(CvAspectRatio);
+
+    const divTag = wrapper.find('div');
+    expect(new Set([...divTag.classes()])).toEqual(
+      new Set([
+        `${carbonPrefix}--aspect-ratio`,
+        `${carbonPrefix}--aspect-ratio--1x1`,
+      ])
+    );
+  });
+
+  it('CvAspectRatio - ratios', async () => {
+    const wrapper = shallowMount(CvAspectRatio);
+
+    const divTag = wrapper.find('div');
+
+    for (const ratio of CvAspectRatioConsts.aspectRatios) {
+      await wrapper.setProps({ ratio });
+      expect(new Set([...divTag.classes()])).toEqual(
+        new Set([
+          `${carbonPrefix}--aspect-ratio`,
+          `${carbonPrefix}--aspect-ratio--${ratio}`,
+        ])
+      );
+    }
+  });
+
+  it('CvAspectRatio - as', async () => {
+    const wrapper = shallowMount(CvAspectRatio);
+    expect(wrapper.element.tagName.toLowerCase()).toEqual('div');
+
+    await wrapper.setProps({ as: 'p' });
+    expect(wrapper.element.tagName.toLowerCase()).toEqual('p');
+  });
+});
