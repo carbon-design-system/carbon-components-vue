d5593a5d5c0f7abedcdf510e1fbdba6e7d4b2809
Timon Lukas <github@timonlukas.de>
2021-04-04 17:07:21 +0200

    Implement component & story

---
+++ packages/v3/src/components/CvList/CvListItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <li :class="`${carbonPrefix}--list__item`"><slot /></li>
+</template>
+
+<script>
+import { carbonPrefix } from '../../global/settings';
+
+export default {
+  name: 'CvListItem',
+  setup: () => ({ carbonPrefix }),
+};
+</script>
