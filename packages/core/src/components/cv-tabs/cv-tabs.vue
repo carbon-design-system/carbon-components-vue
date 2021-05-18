cebf7923000e89a121acf50a4844b2e6e02c6ff5
Lee Chase <leechase@live.com>
2021-05-18 22:27:15 +0100

    Tab1030 (#1193)
    * feat: update tabs to 1030 format
    
    * chore: ci-check
    
    * feat: update tabs disable tests
    
    * fix: tests and remove newer syntax
    
    Co-authored-by: Lee Chase <lee.chase@uk.ibm.com>

--- packages/core/src/components/cv-tabs/cv-tabs.vue
+++ packages/core/src/components/cv-tabs/cv-tabs.vue
@@ -2,56 +2,90 @@
   <div class="cv-tabs" ref="tabs" style="width: 100%;">
     <div
       data-tabs
-      :class="[`cv-tab ${carbonPrefix}--tabs`, { [`${carbonPrefix}--tabs--container`]: container }]"
+      :class="[`cv-tab ${carbonPrefix}--tabs--scrollable`, { [`${carbonPrefix}--tabs--container`]: container }]"
       role="navigation"
       v-on="$listeners"
       v-bind="$attrs"
-      @keydown.right.prevent="onRight"
-      @keydown.left.prevent="onLeft"
-      @keydown.down.prevent="onDown"
-      @keydown.up.prevent="onUp"
-      @keydown.esc.prevent="onEsc"
+      @keydown.right.prevent.stop="onRight"
+      @keydown.left.prevent.stop="onLeft"
     >
-      <div
-        :class="[`${carbonPrefix}--tabs-trigger`, { ' ${carbonPrefix}--tabs-trigger--open': open }]"
-        tabindex="0"
-        ref="trigger"
-        @click="onClick"
-        @keydown.enter.prevent="onClick"
+      <button
+        aria-hidden="true"
+        aria-label="scroll left"
+        :class="[
+          {
+            [`${carbonPrefix}--tab--overflow-nav-button`]: horizontalOverflow,
+            [`${carbonPrefix}--tab--overflow-nav-button--hidden`]: leftOverflowNavButtonHidden,
+          },
+        ]"
+        @click.stop.prevent="e => onOverflowClick(e, { direction: -1 })"
+        @mousedown.stop.prevent="e => onOverflowMouseDown(e, { direction: -1 })"
+        @mouseup.stop.prevent="onOverflowMouseUp"
+        tabIndex="-1"
+        type="button"
+        ref="leftOverflow"
       >
-        <a href="javascript:void(0)" :class="`${carbonPrefix}--tabs-trigger-text`" tabindex="-1">
-          {{ currentTabLabel }}
-        </a>
-        <chevron-down-glyph />
-      </div>
-      <ul :class="[`${carbonPrefix}--tabs__nav`, { [`${carbonPrefix}--tabs__nav--hidden`]: !open }]" role="tablist">
+        <ChevronLeft16 />
+      </button>
+      <div v-if="!leftOverflowNavButtonHidden" :class="`${carbonPrefix}--tabs__overflow-indicator--left`" />
+
+      <ul :class="`${carbonPrefix}--tabs--scrollable__nav`" role="tablist" ref="tablist">
         <li
           v-for="tab in tabs"
           :key="tab.uid"
           :class="[
-            `cv-tabs-button  ${carbonPrefix}--tabs__nav-item`,
+            `cv-tabs-button  ${carbonPrefix}--tabs--scrollable__nav-item`,
             {
               [`${carbonPrefix}--tabs__nav-item--selected`]: selectedId == tab.uid,
               [`${carbonPrefix}--tabs__nav-item--disabled`]: disabledTabs.indexOf(tab.uid) !== -1,
+              // TODO: remove scrollable in next major release
+              [`${carbonPrefix}--tabs--scrollable__nav-item--disabled`]: disabledTabs.indexOf(tab.uid) !== -1,
+              [`${carbonPrefix}--tabs--scrollable__nav-item--selected`]: selectedId == tab.uid,
             },
           ]"
-          role="tab"
+          role="presentation"
           :aria-selected="selectedId == tab.uid ? 'true' : 'false'"
           :aria-disabled="disabledTabs.indexOf(tab.uid) !== -1"
         >
-          <a
-            :class="`${carbonPrefix}--tabs__nav-link`"
-            href="javascript:void(0)"
+          <button
+            :class="`${carbonPrefix}--tabs--scrollable__nav-link`"
             role="tab"
             :aria-controls="tab.uid"
+            :aria-disabled="disabledTabs.indexOf(tab.uid) !== -1"
+            :aria-selected="selectedId == tab.uid"
             :id="`${tab.uid}-link`"
             @click="onTabClick(tab.uid)"
-            @keydown.enter.prevent="onTabEnter(tab.uid)"
             ref="link"
-            >{{ tab.label }}</a
+            eslint-disable-next-line
+            :tabindex="
+              // eslint-disable-nextx-line
+              selectedId == tab.uid ? 0 : -1
+            "
           >
+            {{ tab.label }}
+          </button>
         </li>
       </ul>
+
+      <div v-if="!rightOverflowNavButtonHidden" :class="`${carbonPrefix}--tabs__overflow-indicator--right`" />
+      <button
+        aria-hidden="true"
+        aria-label="scroll right"
+        :class="[
+          {
+            [`${carbonPrefix}--tab--overflow-nav-button`]: horizontalOverflow,
+            [`${carbonPrefix}--tab--overflow-nav-button--hidden`]: rightOverflowNavButtonHidden,
+          },
+        ]"
+        @click="e => onOverflowClick(e, { direction: 1 })"
+        @mousedown="e => onOverflowMouseDown(e, { direction: 1 })"
+        @mouseup="onOverflowMouseUp"
+        tabIndex="-1"
+        type="button"
+        ref="rightOverflow"
+      >
+        <ChevronRight16 />
+      </button>
     </div>
     <div class="cv-tabs__panels">
       <slot></slot>
@@ -60,25 +94,30 @@
 </template>
 
 <script>
-import ChevronDownGlyph from '@carbon/icons-vue/es/chevron--down';
+import { ChevronLeft16, ChevronRight16 } from '@carbon/icons-vue';
 import { carbonPrefixMixin } from '../../mixins';
 
 export default {
   name: 'CvTabs',
   mixins: [carbonPrefixMixin],
   props: {
-    noDefaultToFirst: Boolean,
     container: Boolean,
+    leftOverflowIconButtonProps: Object,
+    noDefaultToFirst: Boolean,
+    rightOverflowIconButtonProps: Object,
+    scrollIntoView: { type: Boolean, default: true },
   },
-  components: { ChevronDownGlyph },
+  components: { ChevronLeft16, ChevronRight16 },
   data() {
     return {
-      tabs: [],
-      selectedId: undefined,
       disabledTabs: [],
-      open: false,
-      lastDisplayProp: undefined,
-      // data is open
+      horizontalOverflow: false,
+      leftOverflowNavButtonHidden: false,
+      rightOverflowNavButtonHidden: false,
+      // scrollIntoView,
+      // selectionMode,
+      selectedId: undefined,
+      tabs: [],
     };
   },
   created() {
@@ -88,20 +127,21 @@ export default {
     this.$on('cv:selected', srcComponent => this.onCvSelected(srcComponent));
     this.$on('cv:disabled', srcComponent => this.onCvDisabled(srcComponent));
     this.$on('cv:enabled', srcComponent => this.onCvEnabled(srcComponent));
+    this.OVERFLOW_BUTTON_OFFSET = 40;
   },
   mounted() {
-    this.$refs.tabs.addEventListener('focusout', this.onFocusout);
-    this.$refs.tabs.addEventListener('focusin', this.onFocusin);
+    window.addEventListener('resize', this.handleResize);
+    this.$refs.tablist.addEventListener('scroll', this.checkScroll);
+    this.checkScroll();
+  },
+  updated() {
+    this.checkScroll();
   },
   beforeDestroy() {
-    this.$refs.tabs.removeEventListener('focusout', this.onFocusout);
-    this.$refs.tabs.removeEventListener('focusin', this.onFocusin);
+    window.removeEventListener('resize', this.handleResize);
+    this.$refs.tablist.removeEventListener('scroll', this.checkScroll);
   },
   computed: {
-    triggerStyleOverride() {
-      // <style carbon tweaks - DO NOT USE STYLE TAG as it causes SSR issues
-      return { padding: 0 };
-    },
     currentTabLabel() {
       const index = this.tabs.findIndex(tab => tab.uid === this.selectedId);
 
@@ -109,51 +149,21 @@ export default {
     },
   },
   methods: {
-    onFocusin(ev) {
-      if (
-        ev.target.classList.contains(`${this.carbonPrefix}--tabs__nav-link`) ||
-        ev.target.classList.contains(`${this.carbonPrefix}--tabs-trigger`)
-      ) {
-        // record display prop state
-        this.lastDisplayProp = window.getComputedStyle(this.$refs.trigger).getPropertyValue('display');
-      } else {
-        this.lastDisplayProp = undefined;
-      }
-    },
-    onFocusout(ev) {
-      // works with onFocusin to determine whether focus needs to be set to a tab or trigger
-      const displayProp = window.getComputedStyle(this.$refs.trigger).getPropertyValue('display');
-      if (ev.relatedTarget) {
-        if (
-          ev.relatedTarget.classList.contains(`${this.carbonPrefix}--tabs__nav-link`) ||
-          ev.relatedTarget.classList.contains(`${this.carbonPrefix}--tabs-trigger`)
-        ) {
-          return; // no need to do anything - focus is going somewhere
-        } else {
-          this.open = false;
-        }
-      } else {
-        if (this.lastDisplayProp && this.lastDisplayProp !== displayProp) {
-          if (displayProp === 'none') {
-            // focus on selected tab
-            const currentTabLink = this.$refs.link.find(link => link.getAttribute('aria-controls') === this.selectedId);
-            if (currentTabLink) {
-              currentTabLink.focus();
-            }
-          } else {
-            this.$refs.trigger.focus();
+    checkScroll() {
+      if (!this.skipScroll) {
+        if (this.$refs.tablist) {
+          this.horizontalOverflow = this.$refs.tablist.scrollWidth > this.$refs.tablist.clientWidth;
+
+          if (this.$refs.link && this.$refs.link.length > 0) {
+            this.leftOverflowNavButtonHidden = this.$refs.tablist.scrollLeft <= 0;
+            this.rightOverflowNavButtonHidden =
+              this.$refs.tablist.scrollLeft + this.$refs.tablist.clientWidth >= this.$refs.tablist.scrollWidth;
           }
-        } else {
-          this.open = false;
         }
       }
     },
-    onWindowResize() {
-      // check whether trigger is displayed
-      this.dataDropdownShown = window.getComputedStyle(this.$refs.trigger).getPropertyValue('display') !== 'none';
-    },
-    onDropChange(val) {
-      this.onTabClick(val);
+    handleResize() {
+      this.checkScroll();
     },
     onCvMount(srcComponent) {
       this.tabs.push(srcComponent);
@@ -166,7 +176,7 @@ export default {
         this.checkSelected();
       } else {
         if (srcComponent.internalSelected) {
-          this.onTabClick(srcComponent.uid);
+          this.doTabClick(srcComponent.uid);
         }
       }
     },
@@ -180,11 +190,51 @@ export default {
         this.checkDisabled(srcComponent);
 
         if (wasSelected && this.tabs.length) {
-          this.onTabClick(this.tabs[Math.max(tabIndex - 1, 0)].uid);
+          this.doTabClick(this.tabs[Math.max(tabIndex - 1, 0)].uid);
         }
       }
     },
-    onTabClick(id) {
+    doScrollIntoView(index) {
+      const tab = this.$refs.link[index];
+      const scrollContainer = tab.parentNode ? tab.parentNode.parentNode : null;
+      let newScrollLeft;
+
+      if (tab && scrollContainer) {
+        const scrollLeft = scrollContainer.scrollLeft;
+        const tabLeft = tab.offsetLeft - scrollContainer.offsetLeft;
+
+        if (index === 0) {
+          newScrollLeft = 0;
+        } else if (index === this.$refs.link.length - 1) {
+          newScrollLeft = scrollContainer.scrollWidth - scrollContainer.offsetWidth;
+        } else if (tabLeft < scrollLeft) {
+          newScrollLeft = tabLeft;
+        } else {
+          const rightOfTab = tab.offsetLeft - scrollContainer.offsetLeft + tab.offsetWidth;
+          if (rightOfTab > scrollContainer.offsetWidth + scrollLeft) {
+            newScrollLeft = rightOfTab - scrollContainer.offsetWidth;
+
+            if (!this.rightOverflowNavButtonHidden) {
+              newScrollLeft += this.OVERFLOW_BUTTON_OFFSET;
+            }
+          }
+        }
+      }
+
+      if (newScrollLeft !== undefined) {
+        this.skipScroll = true;
+        this.leftOverflowNavButtonHidden = newScrollLeft <= 0;
+        this.rightOverflowNavButtonHidden =
+          newScrollLeft + this.$refs.tablist.clientWidth >= this.$refs.tablist.scrollWidth;
+
+        this.$nextTick(() => {
+          // allow left and right nav hide to propegate before setting scroll
+          scrollContainer.scrollLeft = newScrollLeft;
+          this.skipScroll = false;
+        });
+      }
+    },
+    doTabClick(id, setFocus = false) {
       if (this.disabledTabs.indexOf(id) === -1) {
         if (this.selectedId !== id) {
           let newIndex = -1;
@@ -193,6 +243,13 @@ export default {
 
           for (let i = 0; i < this.tabs.length; i++) {
             if (this.tabs[i].uid === id) {
+              this.doScrollIntoView(i, setFocus);
+              if (setFocus) {
+                this.$nextTick(() => {
+                  this.$refs.link[i].focus();
+                });
+              }
+
               this.tabs[i].internalSelected = true;
               newIndex = i;
             } else {
@@ -200,18 +257,15 @@ export default {
             }
           }
 
-          this.open = false;
-          if (this.$refs.trigger) {
-            // following code build sometimes trigger is not yet available
-            this.$refs.trigger.focus();
-          }
-
           this.$emit('tab-selected', newIndex);
         }
       }
     },
+    onTabClick(id) {
+      this.doTabClick(id);
+    },
     onCvSelected(srcComponent) {
-      this.onTabClick(srcComponent.uid);
+      this.doTabClick(srcComponent.uid);
     },
     onCvDisabled(srcComponent) {
       this.disabledTabs.push(srcComponent.uid);
@@ -244,82 +298,22 @@ export default {
 
       if (id !== undefined) {
         this.$nextTick(() => {
-          this.onTabClick(id);
+          this.doTabClick(id);
         });
       }
     },
     isAllTabsDisabled() {
       return this.disabledTabs.length === this.tabs.length;
     },
-    onTabEnter(id) {
-      // const newIndex = this.tabs.findIndex(tab => tab.uid === id);
-
-      this.onTabClick(id);
-      this.$refs.trigger.focus();
-    },
-    onClick() {
-      this.open = !this.open;
-    },
-    onEsc() {
-      this.open = false;
-    },
-    onUp() {
-      if (this.isAllTabsDisabled()) {
-        return;
-      }
-
-      const displayProp = window.getComputedStyle(this.$refs.trigger).getPropertyValue('display');
-
-      if (displayProp !== 'none') {
-        const el = document.activeElement;
-        let id;
-        if (el.classList.contains(`${this.carbonPrefix}--tabs__nav-link`)) {
-          id = el.getAttribute('aria-controls');
-        } else {
-          id = this.selectedId;
-        }
-
-        const newIndex = this.move(id, false);
-        this.$refs.link[newIndex].focus();
-      }
-    },
-    onDown() {
-      if (this.isAllTabsDisabled()) {
-        return;
-      }
-
-      const displayProp = window.getComputedStyle(this.$refs.trigger).getPropertyValue('display');
-
-      if (displayProp !== 'none') {
-        if (!this.open) {
-          this.open = true;
-        } else {
-          const el = document.activeElement;
-          let id;
-          if (el.classList.contains(`${this.carbonPrefix}--tabs__nav-link`)) {
-            id = el.getAttribute('aria-controls');
-          } else {
-            id = this.selectedId;
-          }
-
-          const newIndex = this.move(id, true);
-          this.$refs.link[newIndex].focus();
-        }
-      }
-    },
     onLeft() {
       if (this.isAllTabsDisabled()) {
         return;
       }
 
-      const displayProp = window.getComputedStyle(this.$refs.trigger).getPropertyValue('display');
-      if (displayProp === 'none') {
-        const newIndex = this.move(this.selectedId, false);
-        const newId = this.tabs[newIndex].uid;
+      const curIndex = this.move(this.selectedId, false);
+      const newId = this.tabs[curIndex].uid;
 
-        this.onTabClick(newId);
-        this.$refs.link[newIndex].focus();
-      }
+      this.doTabClick(newId, true);
     },
     move(id, next) {
       let newIndex;
@@ -352,18 +346,65 @@ export default {
         return;
       }
 
-      const displayProp = window.getComputedStyle(this.$refs.trigger).getPropertyValue('display');
-      if (displayProp === 'none') {
-        const newIndex = this.move(this.selectedId, true);
-        const newId = this.tabs[newIndex].uid;
+      const curIndex = this.move(this.selectedId, true);
+      const newId = this.tabs[curIndex].uid;
 
-        this.onTabClick(newId);
-        this.$refs.link[newIndex].focus();
-      }
+      this.doTabClick(newId, true);
     },
     selected(index) {
       let selItem = this.tabs[index ? index : -1];
       this.selectedId = selItem ? selItem.uid : undefined;
+
+      // const selectedTab = this.tabs.find(tab => tab.id === id);
+      // selectedTab.$el.scrollIntoView();
+    },
+    onOverflowClick(e, { direction, multiplier = 10 }) {
+      const { scrollLeft } = this.$refs.tablist;
+
+      // account for overflow button appearing and causing tablist width change
+      if (direction === 1 && !scrollLeft) {
+        this.$refs.tablist.scrollLeft += this.OVERFLOW_BUTTON_OFFSET;
+      }
+
+      this.$refs.tablist.scrollLeft += direction * multiplier;
+
+      if (this.leftEdgeReached(direction)) {
+        this.$refs.tablist.scrollLeft = 0;
+      }
+
+      // account for reaching left edge
+      if (this.leftEdgeReached(direction)) {
+        this.$refs.rightOverflow.focus();
+      }
+      if (this.rightEdgeReached(direction)) {
+        this.$refs.leftOverflow.focus();
+      }
+    },
+    onOverflowMouseDown(e, { direction }) {
+      // disregard mouse buttons aside from LMB
+      if (e.buttons !== 1) {
+        return;
+      }
+
+      this.overflowNavInterval = setInterval(() => {
+        if (this.leftEdgeReached(direction) || this.rightEdgeReached(direction)) {
+          clearInterval(this.overflowNavInterval);
+        }
+
+        this.onOverflowClick(e, { direction });
+      });
+    },
+    onOverflowMouseUp() {
+      clearInterval(this.overflowNavInterval);
+    },
+    leftEdgeReached(direction) {
+      const { scrollLeft } = this.$refs.tablist;
+
+      return direction === -1 && scrollLeft <= this.OVERFLOW_BUTTON_OFFSET;
+    },
+    rightEdgeReached(direction) {
+      const { clientWidth, scrollLeft, scrollWidth } = this.$refs.tablist;
+      return direction === 1 && scrollLeft + clientWidth >= scrollWidth;
     },
   },
 };
