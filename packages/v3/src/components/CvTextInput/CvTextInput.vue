ecca5e22d4c885e373bb79b6d3556603c75ac9d6
Mateus Bandeira <bandeira@ibm.com>
2022-04-01 21:14:19 -0300

    Refactor element classes

--- packages/v3/src/components/CvTextInput/CvTextInput.vue
+++ packages/v3/src/components/CvTextInput/CvTextInput.vue
@@ -1,8 +1,11 @@
 <template>
-  <div :class="wrapperClasses">
+  <div :class="outerWrapperClasses">
     <label :class="labelClasses">{{ label }}</label>
-    <div :class="`${carbonPrefix}--text-input__field-wrapper`" :data-invalid="isInvalid">
-      <WarningFilled16 v-if="isInvalid" :class="`${carbonPrefix}--text-input__invalid-icon`" />
+    <div :class="innerWrapperClasses" :data-invalid="isInvalid">
+      <WarningFilled16
+        v-if="isInvalid"
+        :class="`${carbonPrefix}--text-input__invalid-icon`"
+      />
       <input
         :type="inputType"
         :data-toggle-password-visibility="isPassword"
@@ -12,22 +15,22 @@
       <button
         type="button"
         v-if="isPassword"
-        :class="[
-          `${carbonPrefix}--btn`,
-          `${carbonPrefix}--text-input--password__visibility__toggle`,
-          `${carbonPrefix}--tooltip__trigger`,
-          `${carbonPrefix}--tooltip--a11y`,
-          `${carbonPrefix}--tooltip--bottom`,
-          `${carbonPrefix}--tooltip--align-center`,
-        ]"
+        :class="togglePasswordButtonClasses"
         @click="togglePassword"
       >
-        <span :class="[`${carbonPrefix}--assistive-text`]">{{ togglePasswordLabel }}</span>
-        <ViewOff16 v-if="dataPasswordVisible" :class="`${carbonPrefix}--icon-visibility-off`"></ViewOff16>
-        <View16 v-else :class="`${carbonPrefix}--icon-visibility-off`"></View16>
+        <span :class="`${carbonPrefix}--assistive-text`">{{
+          togglePasswordLabel
+        }}</span>
+        <ViewOff16
+          v-if="dataPasswordVisible"
+          :class="`${carbonPrefix}--icon-visibility-off`"
+        ></ViewOff16>
+        <View16 v-else :class="`${carbonPrefix}--icon-visibility-on`"></View16>
       </button>
     </div>
-    <div v-if="isInvalid" :class="`${carbonPrefix}--form-requirement`">{{ invalidMessage }}</div>
+    <div v-if="isInvalid" :class="`${carbonPrefix}--form-requirement`">
+      {{ invalidMessage }}
+    </div>
     <div v-else :class="helperTextClasses">{{ helperText }}</div>
   </div>
 </template>
@@ -52,14 +55,13 @@ const props = defineProps({
   },
   type: {
     type: String,
-    validator: function (value) {
+    validator: function(value) {
       return ['text', 'password'].includes(value);
-    }
-  }
-})
-
-let isInvalid = computed(() => {
-  return !!props.invalidMessage;
+    },
+  },
+  light: {
+    type: Boolean,
+  },
 });
 
 const inputType = computed(() => {
@@ -71,48 +73,83 @@ const inputType = computed(() => {
   }
 });
 
-const wrapperClasses = computed(() => {
-  const classes = [
-    `cv-text-input`,
-    `${carbonPrefix}--form-item`,
-    `${carbonPrefix}--text-input-wrapper`,
-  ];
-  if (isPassword.value) {
-    classes.push(`${carbonPrefix}--password-input-wrapper`);
-  }
-  return classes;
-});
+function useValidation() {
+  let isInvalid = computed(() => {
+    return !!props.invalidMessage;
+  });
 
-const labelClasses = computed(() => {
-  const classes = [`${carbonPrefix}--label`];
-  if (props.disabled) {
-    classes.push(`${carbonPrefix}--label--disabled`);
-  }
-  if (isPassword.value) {
-    classes.push(`${carbonPrefix}--password-input`);
-  }
-  return classes;
-});
+  return { isInvalid };
+}
 
-const helperTextClasses = computed(() => {
-  const classes = [`${carbonPrefix}--form__helper-text`];
-  if (props.disabled) {
-    classes.push(`${carbonPrefix}--form__helper-text--disabled`);
-  }
-  return classes;
-});
+const { isInvalid } = useValidation();
 
-const inputClasses = computed(() => {
-  const classes = [`${carbonPrefix}--text-input`];
-  if (isInvalid.value) {
-    classes.push(`${carbonPrefix}--text-input--invalid`);
-  }
-  if (isPassword.value) {
-    classes.push(`${carbonPrefix}--password-input`);
-  }
-  return classes;
-});
+function useClasses() {
+  const outerWrapperClasses = computed(() => {
+    const classes = [
+      `cv-text-input`,
+      `${carbonPrefix}--form-item`,
+      `${carbonPrefix}--text-input-wrapper`,
+    ];
+    if (isPassword.value) {
+      classes.push(`${carbonPrefix}--password-input-wrapper`);
+    }
+    return classes;
+  });
 
+  const labelClasses = computed(() => {
+    const classes = [`${carbonPrefix}--label`];
+    if (props.disabled) {
+      classes.push(`${carbonPrefix}--label--disabled`);
+    }
+    if (isPassword.value) {
+      classes.push(`${carbonPrefix}--password-input`);
+    }
+    return classes;
+  });
+
+  const innerWrapperClasses = computed(() => {
+    const classes = [`${carbonPrefix}--text-input__field-wrapper`];
+    return classes;
+  });
+
+  const helperTextClasses = computed(() => {
+    const classes = [`${carbonPrefix}--form__helper-text`];
+    if (props.disabled) {
+      classes.push(`${carbonPrefix}--form__helper-text--disabled`);
+    }
+    return classes;
+  });
+
+  const inputClasses = computed(() => {
+    const classes = [`${carbonPrefix}--text-input`];
+    if (isInvalid.value) {
+      classes.push(`${carbonPrefix}--text-input--invalid`);
+    }
+    if (isPassword.value) {
+      classes.push(`${carbonPrefix}--password-input`);
+    }
+    if (props.light) {
+      classes.push(`${carbonPrefix}--text-input--light`);
+    }
+    return classes;
+  });
+
+  return {
+    outerWrapperClasses,
+    labelClasses,
+    innerWrapperClasses,
+    helperTextClasses,
+    inputClasses,
+  };
+}
+
+const {
+  outerWrapperClasses,
+  labelClasses,
+  innerWrapperClasses,
+  helperTextClasses,
+  inputClasses,
+} = useClasses();
 
 function usePasswordFeature() {
   let dataPasswordVisible = ref(false);
@@ -123,16 +160,40 @@ function usePasswordFeature() {
 
   let togglePasswordLabel = computed(() => {
     if (dataPasswordVisible.value) {
-      return "Hide password";
+      return 'Hide password';
     } else {
-      return "Show password"
+      return 'Show password';
     }
   });
 
   let togglePassword = () => {
     dataPasswordVisible.value = !dataPasswordVisible.value;
   };
-  return { dataPasswordVisible, isPassword, togglePasswordLabel, togglePassword };
+
+  const togglePasswordButtonClasses = computed(() => {
+    const classes = [
+      `${carbonPrefix}--btn`,
+      `${carbonPrefix}--text-input--password__visibility__toggle`,
+      `${carbonPrefix}--tooltip__trigger`,
+      `${carbonPrefix}--tooltip--a11y`,
+      `${carbonPrefix}--tooltip--bottom`,
+      `${carbonPrefix}--tooltip--align-center`,
+    ];
+    return classes;
+  });
+  return {
+    dataPasswordVisible,
+    isPassword,
+    togglePasswordLabel,
+    togglePassword,
+    togglePasswordButtonClasses,
+  };
 }
-const { dataPasswordVisible, isPassword, togglePasswordLabel, togglePassword } = usePasswordFeature();
+const {
+  dataPasswordVisible,
+  isPassword,
+  togglePasswordLabel,
+  togglePassword,
+  togglePasswordButtonClasses,
+} = usePasswordFeature();
 </script>
