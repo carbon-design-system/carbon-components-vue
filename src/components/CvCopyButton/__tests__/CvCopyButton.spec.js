60858847296f388e91f253d77085715cf5d4107b
Lee Chase <lee.chase@uk.ibm.com>
2022-05-10 18:38:35 +0100

    fix: tests

--- src/components/CvCopyButton/__tests__/CvCopyButton.spec.js
+++ src/components/CvCopyButton/__tests__/CvCopyButton.spec.js
@@ -8,9 +8,7 @@ describe('CvCopyButton', () => {
   it('raises copy event when clicked', async () => {
     const wrapper = mount(CvCopyButton);
     await wrapper.find('button').trigger('click');
-    // `copy` will have been triggered twice because the inner element triggers it
-    // and is technically on the same wrapper
-    expect(wrapper.emitted('copy')).toHaveLength(2);
+    expect(wrapper.emitted('copy')).toHaveLength(1);
   });
 
   it('fades a feedback element in and out', async () => {
