1b2160340880882dbd26ad201826f158227275c6
David Nixon <david_nixon@us.ibm.com>
2022-11-28 22:33:15 -0500

    chore: change the story from .js to .mdx. So much easier to add docs!

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
