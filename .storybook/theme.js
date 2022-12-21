21b978b84ed2b60544ae1d3e60a6862d96eef636
David Nixon <david_nixon@us.ibm.com>
2022-12-21 15:46:41 -0500

    chore: update storybook to match react styles

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,21 +1,25 @@
+/**
+ * Copyright IBM Corp. 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { create } from '@storybook/theming';
 
-const theme = create({
-  base: 'dark',
-  colorPrimary: '#8a3ffc', // purple 60
-  colorSecondary: '#78a9ff', // blue 40
-  appBg: '#262626', // gray 90
-  barBg: '#e0e0e0', // gray 20
-  barTextColor: '#161616', // gray 100
-  barSelectedColor: '#0f62fe', // blue 60
-  inputBg: '#262626',
-  inputBorder: '#0f62fe',
+/**
+ * @see https://storybook.js.org/docs/react/configure/theming
+ */
+export default create({
+  base: 'light',
+
+  // Typography
+  fontBase: "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif",
+  fontCode:
+    "'IBM Plex Mono', Menlo, 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', Courier, monospace",
+
   brandTitle: '@carbon/vue3',
   brandUrl:
     'https://github.com/carbon-design-system/carbon-components-vue/tree/main/packages/v3',
-  fontBase: '"IBM Plex Sans", "Helvetica Neue", Arial, sans-serif',
-  fontCode:
-    '"IBM Plex Mono", "Menlo", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", Courier, monospace',
   brandTarget: '_blank',
 });
-export default theme;
