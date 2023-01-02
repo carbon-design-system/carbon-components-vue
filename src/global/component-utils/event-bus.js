1657d64af7c82eece0925e9c3197c5235b9247c3
David Nixon <david_nixon@us.ibm.com>
2023-01-02 17:39:40 -0500

    chore: update dependencies & tweak event bus

--- src/global/component-utils/event-bus.js
+++ src/global/component-utils/event-bus.js
@@ -1,22 +1,25 @@
 import { EventEmitter } from 'events';
+import { unref } from 'vue';
 
 class EventBus extends EventEmitter {}
 
 const busses = {};
 /**
  * Get an event bus
- * @param {string} id unique id
+ * @param {string} busId unique id
  * @returns {EventBus}
  */
-export function getBus(id) {
+export function getBus(busId) {
+  const id = unref(busId);
   if (!(id in busses)) busses[id] = new EventBus();
   return busses[id];
 }
 /**
  * Remove a previously created event bus
- * @param {string} id unique id
+ * @param {string} busId unique id
  * @returns {EventBus}
  */
-export function removeBus(id) {
+export function removeBus(busId) {
+  const id = unref(busId);
   delete busses[id];
 }
