f4a3af4af5ef32bc9c667035568f1a6d52b547da
Eduardo França <edox86@gmail.com>
2023-01-05 23:44:22 -0300

    using native crypto.randomUUID method

--- packages/core/src/mixins/uid-mixin.js
+++ packages/core/src/mixins/uid-mixin.js
@@ -1,12 +1,6 @@
 const idGen = {
   newId() {
-    // as it is funny gen IDs using b - https://gist.github.com/jed/982883
-    const b = a =>
-      a
-        ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
-        : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b);
-
-    return `uid-${b()}`;
+    return `uid-${ crypto.randomUUID() }`;
   },
 };
 
