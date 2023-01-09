16e412b2c3e9a587c3d4ad0ad3afcb0a9b861c66
David Nixon <david_nixon@us.ibm.com>
2023-01-09 15:40:34 -0500

    chore: make v3 installable as a branch

--- main.js
+++ main.js
@@ -1,37 +1,76 @@
-export CvAspectRatio from './src/components/CvAspectRatio/CvAspectRatio.vue';
-export CvListItem from './src/components/CvList/CvListItem.vue';
-export CvList from './src/components/CvList/CvList.vue';
-export CvOverflowMenuItem from './src/components/CvOverflowMenu/CvOverflowMenuItem.vue';
-export CvOverflowMenu from './src/components/CvOverflowMenu/CvOverflowMenu.vue';
-export CvButtonSet from './src/components/CvButton/CvButtonSet.vue';
-export CvIconButton from './src/components/CvButton/CvIconButton.vue';
-export CvButton from './src/components/CvButton/CvButton.vue';
-export CvAccordionItem from './src/components/CvAccordion/CvAccordionItem.vue';
-export CvAccordion from './src/components/CvAccordion/CvAccordion.vue';
-export CvTag from './src/components/CvTag/CvTag.vue';
-export CvComboBox from './src/components/CvComboBox/CvComboBox.vue';
-export CvPagination from './src/components/CvPagination/CvPagination.vue';
-export CvCopyButton from './src/components/CvCopyButton/CvCopyButton.vue';
-export CvSelectOptgroup from './src/components/CvSelect/CvSelectOptgroup.vue';
-export CvSelect from './src/components/CvSelect/CvSelect.vue';
-export CvSelectOption from './src/components/CvSelect/CvSelectOption.vue';
-export CvCodeSnippetSkeleton from './src/components/CvCodeSnippet/CvCodeSnippetSkeleton.vue';
-export CvCodeSnippet from './src/components/CvCodeSnippet/CvCodeSnippet.vue';
-export CvDataTableRow from './src/components/CvDataTable/CvDataTableRow.vue';
-export CvDataTableAction from './src/components/CvDataTable/CvDataTableAction.vue';
-export CvDataTable from './src/components/CvDataTable/CvDataTable.vue';
-export CvDataTableCell from './src/components/CvDataTable/CvDataTableCell.vue';
-export CvDataTableSkeleton from './src/components/CvDataTable/CvDataTableSkeleton.vue';
-export CvDataTableHeading from './src/components/CvDataTable/CvDataTableHeading.vue';
-export CvBreadcrumb from './src/components/CvBreadcrumb/CvBreadcrumb.vue';
-export CvBreadcrumbSkeletonItem from './src/components/CvBreadcrumb/CvBreadcrumbSkeletonItem.vue';
-export CvBreadcrumbItem from './src/components/CvBreadcrumb/CvBreadcrumbItem.vue';
-export CvBreadcrumbSkeleton from './src/components/CvBreadcrumb/CvBreadcrumbSkeleton.vue';
-export CvContentSwitcher from './src/components/CvContentSwitcher/CvContentSwitcher.vue';
-export CvContentSwitcherButton from './src/components/CvContentSwitcher/CvContentSwitcherButton.vue';
-export CvContentSwitcherContent from './src/components/CvContentSwitcher/CvContentSwitcherContent.vue';
-export CvSkeletonText from './src/components/CvSkeletonText/CvSkeletonText.vue';
-export CvInlineNotification from './src/components/CvNotification/CvInlineNotification.vue';
-export CvToastNotification from './src/components/CvNotification/CvToastNotification.vue';
-export CvLoading from './src/components/CvLoading/CvLoading.vue';
-export CvCheckbox from './src/components/CvCheckbox/CvCheckbox.vue';
+import CvAspectRatio from './src/components/CvAspectRatio/CvAspectRatio.vue';
+import CvListItem from './src/components/CvList/CvListItem.vue';
+import CvList from './src/components/CvList/CvList.vue';
+import CvOverflowMenuItem from './src/components/CvOverflowMenu/CvOverflowMenuItem.vue';
+import CvOverflowMenu from './src/components/CvOverflowMenu/CvOverflowMenu.vue';
+import CvButtonSet from './src/components/CvButton/CvButtonSet.vue';
+import CvIconButton from './src/components/CvButton/CvIconButton.vue';
+import CvButton from './src/components/CvButton/CvButton.vue';
+import CvAccordionItem from './src/components/CvAccordion/CvAccordionItem.vue';
+import CvAccordion from './src/components/CvAccordion/CvAccordion.vue';
+import CvTag from './src/components/CvTag/CvTag.vue';
+import CvComboBox from './src/components/CvComboBox/CvComboBox.vue';
+import CvPagination from './src/components/CvPagination/CvPagination.vue';
+import CvCopyButton from './src/components/CvCopyButton/CvCopyButton.vue';
+import CvSelectOptgroup from './src/components/CvSelect/CvSelectOptgroup.vue';
+import CvSelect from './src/components/CvSelect/CvSelect.vue';
+import CvSelectOption from './src/components/CvSelect/CvSelectOption.vue';
+import CvCodeSnippetSkeleton from './src/components/CvCodeSnippet/CvCodeSnippetSkeleton.vue';
+import CvCodeSnippet from './src/components/CvCodeSnippet/CvCodeSnippet.vue';
+import CvDataTableRow from './src/components/CvDataTable/CvDataTableRow.vue';
+import CvDataTableAction from './src/components/CvDataTable/CvDataTableAction.vue';
+import CvDataTable from './src/components/CvDataTable/CvDataTable.vue';
+import CvDataTableCell from './src/components/CvDataTable/CvDataTableCell.vue';
+import CvDataTableSkeleton from './src/components/CvDataTable/CvDataTableSkeleton.vue';
+import CvDataTableHeading from './src/components/CvDataTable/CvDataTableHeading.vue';
+import CvBreadcrumb from './src/components/CvBreadcrumb/CvBreadcrumb.vue';
+import CvBreadcrumbSkeletonItem from './src/components/CvBreadcrumb/CvBreadcrumbSkeletonItem.vue';
+import CvBreadcrumbItem from './src/components/CvBreadcrumb/CvBreadcrumbItem.vue';
+import CvBreadcrumbSkeleton from './src/components/CvBreadcrumb/CvBreadcrumbSkeleton.vue';
+import CvContentSwitcher from './src/components/CvContentSwitcher/CvContentSwitcher.vue';
+import CvContentSwitcherButton from './src/components/CvContentSwitcher/CvContentSwitcherButton.vue';
+import CvContentSwitcherContent from './src/components/CvContentSwitcher/CvContentSwitcherContent.vue';
+import CvSkeletonText from './src/components/CvSkeletonText/CvSkeletonText.vue';
+import CvInlineNotification from './src/components/CvNotification/CvInlineNotification.vue';
+import CvToastNotification from './src/components/CvNotification/CvToastNotification.vue';
+import CvLoading from './src/components/CvLoading/CvLoading.vue';
+import CvCheckbox from './src/components/CvCheckbox/CvCheckbox.vue';
+export {
+  CvAspectRatio,
+  CvListItem,
+  CvList,
+  CvOverflowMenuItem,
+  CvOverflowMenu,
+  CvButtonSet,
+  CvIconButton,
+  CvButton,
+  CvAccordionItem,
+  CvAccordion,
+  CvTag,
+  CvComboBox,
+  CvPagination,
+  CvCopyButton,
+  CvSelectOptgroup,
+  CvSelect,
+  CvSelectOption,
+  CvCodeSnippetSkeleton,
+  CvCodeSnippet,
+  CvDataTableRow,
+  CvDataTableAction,
+  CvDataTable,
+  CvDataTableCell,
+  CvDataTableSkeleton,
+  CvDataTableHeading,
+  CvBreadcrumb,
+  CvBreadcrumbSkeletonItem,
+  CvBreadcrumbItem,
+  CvBreadcrumbSkeleton,
+  CvContentSwitcher,
+  CvContentSwitcherButton,
+  CvContentSwitcherContent,
+  CvSkeletonText,
+  CvInlineNotification,
+  CvToastNotification,
+  CvLoading,
+  CvCheckbox,
+};
