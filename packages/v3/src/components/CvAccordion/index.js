9411c178820d702e1787f24bffc086dce293e18a
David Nixon <david_nixon@us.ibm.com>
2023-01-09 11:14:23 -0500

    chore: remove v3 from main branch

--- packages/v3/src/components/CvAccordion/index.js
+++
@@ -1,6 +0,0 @@
-import CvAccordion from './CvAccordion';
-import CvAccordionItem from './CvAccordionItem';
-import CvAccordionConsts from './consts';
-
-export { CvAccordion, CvAccordionItem, CvAccordionConsts };
-export default CvAccordion;
--- packages/v3/src/components/CvCodeSnippet/index.js
+++
@@ -1,3 +0,0 @@
-export { default, default as CvCodeSnippet } from './CvCodeSnippet';
-export { default as CvCodeSnippetConsts } from './consts';
-export { default as CvCodeSnippetSkeleton } from './CvCodeSnippetSkeleton';
