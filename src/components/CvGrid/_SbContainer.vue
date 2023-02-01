9a136fceb3335048ef3d263a9cef3b9ccda4baa8
David Nixon <david_nixon@us.ibm.com>
2023-02-01 09:41:06 -0500

    fix: make storybook styles more like react

--- src/components/CvGrid/_SbContainer.vue
+++ src/components/CvGrid/_SbContainer.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="cv-grid-story">
+  <div class="sb-grid-container">
     <slot></slot>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss">
-@import '@carbon/grid/scss/modules/css-grid';
-.cv-grid-story {
+<style>
+.sb-grid-container {
   width: 100%;
 }
-.cv-grid-story .cv-column {
+.sb-grid-container .cv-column {
   background-color: #d0e2ff; /* blue-20 */
   outline: 1px dashed #78a9ff; /* blue-40 */
 }
-.cv-column div {
+.sb-grid-container .cv-column div {
   background-color: #edf5ff; /* blue-10 */
   min-height: 100px;
   height: 100%;
