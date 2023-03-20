2007b9bf5cbe9f4aaac7a5628e8432f4dcd64d68
Felipe Brito <felipebritor@gmail.com>
2023-03-19 21:06:42 -0300

    feat: add CvLink component
    CvLink component implementation covering all functionalities available at vue-carbon@2

---
+++ src/components/CvLink/CvLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <component
+    :is="tagType"
+    v-bind="linkProps"
+    class="cv-link"
+    :class="[
+      `${carbonPrefix}--link`,
+      {
+        [`${carbonPrefix}--link--disabled`]: disabled,
+        [`${carbonPrefix}--link--inline`]: inline,
+      },
+    ]"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<script setup>
+import { carbonPrefix } from '../../global/settings';
+import {
+  props as linkPropsDefinition,
+  useLinkProps,
+  useTagType,
+} from '../../use/cvLink';
+
+const props = defineProps({
+  inline: Boolean,
+  ...linkPropsDefinition,
+});
+const linkProps = useLinkProps(props);
+const tagType = useTagType(props);
+</script>
