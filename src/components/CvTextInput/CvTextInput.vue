60e7b5a03b1214614530cd94f492f83caa035311
Felipe Brito <felipebritor@gmail.com>
2023-04-10 14:13:35 -0300

    feat(text-input): add input & 'v-model' capabilities

--- src/components/CvTextInput/CvTextInput.vue
+++ src/components/CvTextInput/CvTextInput.vue
@@ -9,6 +9,14 @@
     <label :class="[`${carbonPrefix}--label`]">
       {{ label }}
     </label>
+    <div :class="[`${carbonPrefix}--text-input__field-wrapper`]">
+      <input
+        :class="[`${carbonPrefix}--text-input`]"
+        type="text"
+        :value="modelValue"
+        @input="$event => $emit('update:modelValue', $event.target.value)"
+      />
+    </div>
   </div>
 </template>
 
@@ -17,5 +25,8 @@ import { carbonPrefix } from '../../global/settings';
 
 const props = defineProps({
   label: String,
+  modelValue: String,
 });
+
+const emit = defineEmits(['update:modelValue']);
 </script>
--- src/components/CvTextInput/__tests__/CvTextInput.spec.js
+++ src/components/CvTextInput/__tests__/CvTextInput.spec.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
 import CvTextInput from '..';
 
 describe('CvTextInput', () => {
@@ -11,5 +12,28 @@ describe('CvTextInput', () => {
     const label = getByText(dummyLabel);
     expect(label.tagName).toBe('LABEL');
     expect(label.textContent).toBe(dummyLabel);
+  });
+
+  it('emits model value on native input update', async () => {
+    const dummyText = 'Lorem Ipsum';
+    const user = userEvent.setup();
+    const { container, emitted } = render(CvTextInput);
+
+    const input = container.querySelector('input');
+    await user.type(input, dummyText);
+
+    const updateEvent = emitted()['update:modelValue'];
+    expect(updateEvent.length).toBe(dummyText.length);
+    expect(updateEvent[updateEvent.length - 1]).toEqual([dummyText]);
+  });
+
+  it("updates native input with 'value' prop", async () => {
+    const dummyInitialText = 'Lorem Ipsum';
+    const { container } = render(CvTextInput, {
+      props: { modelValue: dummyInitialText },
+    });
+
+    const input = container.querySelector('input');
+    expect(input.value).toBe(dummyInitialText);
   });
 });
