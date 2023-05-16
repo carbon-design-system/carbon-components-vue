d93b8256b650ef75a65797bd9dbf5aec0214df44
Felipe <felipebritor@gmail.com>
2023-05-16 19:11:42 -0300

    feat: implement CvForm, CvFormGroup and CvFormItem (#1452)
    * feat(cv-form): add CvForm component
    
    * feat(cv-form-group): add CvFormGroup component
    
    * feat(cv-form-item): add CvFormItem component
    
    * feat: add bottom margin to bx--form-item elements
    
    Repeat the styling used on v2 for form item element class to give
    them some spacing at the stories
    
    * docs: add stories for form, form-group and form-item components

---
+++ src/components/CvForm/CvForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <form :class="`cv-form ${carbonPrefix}--form`">
+    <slot></slot>
+  </form>
+</template>
+
+<script setup>
+import { carbonPrefix } from '../../global/settings';
+</script>
---
+++ src/components/CvForm/index.js
@@ -0,0 +1,5 @@
+import CvForm from './CvForm';
+import CvFormGroup from './CvFormGroup';
+import CvFormItem from './CvFormItem';
+
+export { CvForm, CvFormGroup, CvFormItem };
