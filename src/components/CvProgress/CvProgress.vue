3eb87a1086a4f9038424166dabc8abc0e5d9a8cd
David Nixon <david_nixon@us.ibm.com>
2023-07-28 11:33:21 -0400

    feat: compoent working. needs tests

---
+++ src/components/CvProgress/CvProgress.vue
@@ -0,0 +1,76 @@
+<template>
+  <ul
+    data-progress
+    data-progress-current
+    :class="[
+      `cv-progress ${carbonPrefix}--progress`,
+      { [`${carbonPrefix}--progress--space-equal`]: isEqual },
+      { [`${carbonPrefix}--progress--vertical`]: vertical },
+    ]"
+  >
+    <slot>
+      <cv-progress-step
+        v-for="(step, index) in steps"
+        :key="`step:${index}`"
+        :label="step"
+        :complete="initialStep > index"
+        @step-clicked="emit('step-clicked', { ...$event, index })"
+      />
+    </slot>
+  </ul>
+</template>
+
+<script setup>
+// noinspection ES6PreferShortImport
+import { carbonPrefix } from '../../global/settings';
+import CvProgressStep from './CvProgressStep.vue';
+import { onMounted, provide, ref } from 'vue';
+
+const props = defineProps({
+  /**
+   * Optionally specify the current step array index
+   */
+  initialStep: { type: Number, default: 0 },
+  /**
+   * An array of labels for the steps. Use `cv-progress-step` components for more control over the step logic.
+   */
+  steps: Array,
+  /**
+   * Determines whether the ProgressIndicator should be rendered vertically.
+   */
+  vertical: { type: Boolean, default: false },
+  /**
+   * Specify whether the progress steps should be split equally in size in the div
+   */
+  spaceEqually: { type: Boolean, default: false },
+});
+const emit = defineEmits(['step-clicked']);
+
+const isEqual = ref(props.spaceEqually && !props.vertical);
+provide('progress-space-equally', isEqual);
+const completedSteps = ref(new Set());
+provide('progress-completed-steps', completedSteps);
+
+/**
+ * @typedef ProgressStepData
+ * @type {string} uid
+ * @type {string} label
+ */
+/**
+ * @type {Ref<UnwrapRef<Array<ProgressStepData>>>}
+ */
+const progressSteps = ref([]);
+provide('progress-steps', progressSteps);
+
+const currentStep = ref('');
+provide('progress-current-step', currentStep);
+onMounted(() => {
+  if (
+    props.initialStep > -1 &&
+    props.initialStep < progressSteps.value.length
+  ) {
+    const step = progressSteps.value[props.initialStep];
+    currentStep.value = step.uid;
+  }
+});
+</script>
