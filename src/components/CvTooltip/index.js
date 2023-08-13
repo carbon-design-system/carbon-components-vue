6cd072ffce584a8d95c8a7edb7ca50908182b658
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-08-13 09:03:28 +0100

    feat: cv-tooltip export static values

--- src/components/CvTooltip/index.js
+++ src/components/CvTooltip/index.js
@@ -2,4 +2,5 @@ import CvTooltip from './CvTooltip.vue';
 import CvDefinitionTooltip from './CvDefinitionTooltip.vue';
 import CvInteractiveTooltip from './CvInteractiveTooltip.vue';
 export { CvTooltip, CvDefinitionTooltip, CvInteractiveTooltip };
+export { TipAlignments, TipDirections, alignments, directions } from './consts';
 export default CvTooltip;
