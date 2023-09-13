07fd62bf726534e46bf65bc4abeb9ca92aa6ab58
Caio <caiofernandes243@gmail.com>
2023-09-13 15:34:41 -0300

    feat: add CvTagSkeleton component and it's Storybook

---
+++ src/components/CvTag/CvTagSkeleton.stories.js
@@ -0,0 +1,39 @@
+import {
+  sbCompPrefix,
+  storyParametersObject,
+} from '../../global/storybook-utils';
+import CvTagSkeleton from './CvTagSkeleton.vue';
+
+export default {
+  title: `${sbCompPrefix}/CvTagSkeleton`,
+  component: CvTagSkeleton,
+};
+
+const template = `<cv-tag-skeleton v-bind="args"></cv-tag-skeleton>`;
+const Template = (args, { argTypes }) => {
+  return {
+    props: Object.keys(argTypes),
+    components: { CvTagSkeleton },
+    template,
+    setup() {
+      return { args };
+    },
+  };
+};
+
+export const Default = Template.bind({});
+Default.parameters = storyParametersObject(
+  Default.parameters,
+  template,
+  Default.args
+);
+
+export const Small = Template.bind({});
+Small.args = {
+  small: true,
+};
+Small.parameters = storyParametersObject(
+  Small.parameters,
+  template,
+  Small.args
+);
