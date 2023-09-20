1e28477c2d5ea5c70333c8449eaacc492ff0a254
Felipe Brito <felipebritor@gmail.com>
2023-09-20 15:26:31 -0300

    feat(storybook): add hack to setup custom controls
    This is a fragile **workaround** to customize new storybook controls.
    
    Currently, storybook lack the option to create customizable controls, as an option
    for knobs. This hack allow us to define our own control types, that should be
    defined at `window["STORYBOOK_CUSTOM_CONTROLS"]`, created at manager.js.
    
    This solution, along with the button control defined at manager.js, was found in
    a discussion about a new button control type:
    https://github.com/storybookjs/storybook/issues/11971#issuecomment-1240831460

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -7,6 +7,8 @@
 
 import { addons } from '@storybook/addons';
 import theme from './theme';
+import React from 'react';
+import { Form } from '@storybook/components';
 
 addons.setConfig({
   theme,
@@ -16,3 +18,24 @@ addons.setConfig({
 // conditional panels, or other things that get in the way of our workflow
 localStorage.removeItem('@storybook/ui/store');
 localStorage.removeItem('storybook-layout');
+
+/**
+ * This is a **workaround** due to the lack of a 'button' control in storybook.
+ * It relies on postinstall.js defining `window["STORYBOOK_CUSTOM_CONTROLS"]`
+ * as a fallback object, at @storybook/components module, when storybook looks
+ * for a control type.
+ */
+window['STORYBOOK_CUSTOM_CONTROLS'] = {
+  button({ argType, value, onChange }) {
+    return React.createElement(
+      Form.Button,
+      {
+        onClick() {
+          value = typeof value === 'number' ? value : 0;
+          onChange(value + 1);
+        },
+      },
+      argType.buttonLabel || argType.name
+    );
+  },
+};
