1a341b1993b29dc8d18d24daa7c31090754bc929
Mateus Bandeira <bandeira@ibm.com>
2023-10-11 20:01:59 -0300

    feat(cv-text-input-skeleton): add initial skeleton implementation

---
+++ src/components/CvTextInput/CvTextInputSkeleton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="[`${carbonPrefix}--form-item`]">
+    <span
+      :class="[`${carbonPrefix}--label`, `${carbonPrefix}--skeleton`]"
+    ></span>
+    <div
+      :class="[`${carbonPrefix}--text-input`, `${carbonPrefix}--skeleton`]"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+import { carbonPrefix } from '../../global/settings';
+</script>
