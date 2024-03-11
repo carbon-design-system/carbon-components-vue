f2f42e8f4ee7a3a9eb9d45dbce14e2716d666611
David Nixon <david_nixon@us.ibm.com>
2024-03-11 11:58:01 -0400

    chore: add source code to cv-toast-notification story

--- src/components/CvNotification/CvToastNotification.stories.js
+++ src/components/CvNotification/CvToastNotification.stories.js
@@ -1,6 +1,6 @@
 import { action } from '@storybook/addon-actions';
 import DocumentationTemplate from './CvToastNotificationTemplate.mdx';
-import { sbCompPrefix } from '../../global/storybook-utils';
+import { sbCompPrefix, storySourceCode } from '../../global/storybook-utils';
 import { CvToastNotification, CvNotificationConsts } from '.';
 
 export default {
@@ -123,58 +123,113 @@ const Template = args => {
   };
 };
 
-export const Info = Template.bind({});
-Info.args = {
+export const Default = Template.bind({});
+Default.args = {
   kind: CvNotificationConsts.notificationKinds[0],
+  title: 'info',
+  subTitle: 'subtitle',
+  caption: 'more information',
+};
+Default.parameters = {
+  docs: { source: { code: storySourceCode(template, Default.args) } },
 };
 
 export const InfoSquare = Template.bind({});
 InfoSquare.storyName = 'Info square';
 InfoSquare.args = {
   kind: CvNotificationConsts.notificationKinds[1],
+  title: 'info²',
+  subTitle: 'subtitle',
+  caption: 'more information',
+};
+InfoSquare.parameters = {
+  docs: { source: { code: storySourceCode(template, InfoSquare.args) } },
 };
 
 export const Success = Template.bind({});
 Success.args = {
   kind: CvNotificationConsts.notificationKinds[2],
+  title: 'success',
+  subTitle: 'subtitle',
+  caption: 'more information',
 };
-
+Success.parameters = {
+  docs: { source: { code: storySourceCode(template, Success.args) } },
+};
 export const Warning = Template.bind({});
 Warning.args = {
   kind: CvNotificationConsts.notificationKinds[3],
+  title: 'warning',
+  subTitle: 'subtitle',
+  caption: 'more information',
+};
+Warning.parameters = {
+  docs: { source: { code: storySourceCode(template, Warning.args) } },
 };
 
 export const WarningAlt = Template.bind({});
 WarningAlt.storyName = 'Warning (alt)';
 WarningAlt.args = {
   kind: CvNotificationConsts.notificationKinds[4],
+  title: 'warning △',
+  subTitle: 'subtitle',
+  caption: 'more information',
+};
+WarningAlt.parameters = {
+  docs: { source: { code: storySourceCode(template, WarningAlt.args) } },
 };
 
 export const Error = Template.bind({});
 Error.args = {
   kind: CvNotificationConsts.notificationKinds[5],
+  title: 'error',
+  subTitle: 'subtitle',
+  caption: 'more information',
+};
+Error.parameters = {
+  docs: { source: { code: storySourceCode(template, Error.args) } },
 };
 
 export const SlotDefault = Template.bind({});
 SlotDefault.args = {
   kind: CvNotificationConsts.notificationKinds[4],
+  title: 'title',
+  subTitle: 'subtitle',
   defaultSlot: true,
+};
+SlotDefault.parameters = {
+  docs: { source: { code: storySourceCode(template, SlotDefault.args) } },
 };
 
 export const SlotTitle = Template.bind({});
 SlotTitle.args = {
   kind: CvNotificationConsts.notificationKinds[0],
   titleSlot: true,
+  subTitle: 'subtitle',
+  caption: 'more information',
+};
+SlotTitle.parameters = {
+  docs: { source: { code: storySourceCode(template, SlotTitle.args) } },
 };
 
 export const SlotSubtitle = Template.bind({});
 SlotSubtitle.args = {
   kind: CvNotificationConsts.notificationKinds[3],
   subtitleSlot: true,
+  title: 'title',
+  caption: 'more information',
+};
+SlotSubtitle.parameters = {
+  docs: { source: { code: storySourceCode(template, SlotSubtitle.args) } },
 };
 
 export const SlotCaption = Template.bind({});
 SlotCaption.args = {
   kind: CvNotificationConsts.notificationKinds[1],
   captionSlot: true,
+  title: 'title',
+  subTitle: 'subtitle',
+};
+SlotCaption.parameters = {
+  docs: { source: { code: storySourceCode(template, SlotCaption.args) } },
 };
