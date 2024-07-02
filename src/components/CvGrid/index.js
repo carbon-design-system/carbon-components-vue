15b8daa26bf224ea58bec59015a8ec142ff6575e
Oscar Zambotti <oskarnrk@users.noreply.github.com>
2024-07-02 10:32:13 +0200

    Fix import in CvGrid/index.js

--- src/components/CvGrid/index.js
+++ src/components/CvGrid/index.js
@@ -1,5 +1,5 @@
 import CvColumn from './CvColumn.vue';
-import CvGrid from './CvFormGroup.vue';
+import CvGrid from './CvGrid.vue';
 import CvRow from './CvRow.vue';
 
 export { CvColumn, CvGrid, CvRow };
