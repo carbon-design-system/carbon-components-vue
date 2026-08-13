5f76e8a8c1f82e0f19f6f3a94c75ef344b761f55
Guilherme G. Menaldo <--local>
2026-08-13 13:59:01 -0300

    refactor: migrate CvCopyButton to new storybook format
    - stories.js keeps the component setup
    - .mdx keeps the Docs page

--- src/components/CvCopyButton/CvCopyButton.stories.js
+++ src/components/CvCopyButton/CvCopyButton.stories.js
@@ -1,7 +1,7 @@
+import { computed } from 'vue';
 import CvCopyButton from '.';
 
 import { action } from '@storybook/addon-actions';
-import { storyParametersObject } from '../../global/storybook-utils';
 
 export default {
   title: 'Component/CvCopyButton',
@@ -10,16 +10,16 @@ export default {
 };
 
 const template = `<cv-copy-button v-bind="args" @copy="copy" />`;
-const Template = args => ({
-  components: { CvCopyButton },
-  setup: () => ({ args, copy: action('copy') }),
-  template,
+const Template = argsIn => ({
+  args: argsIn,
+  render: args => ({
+    components: { CvCopyButton },
+    setup: () => ({
+      args: computed(() => args),
+      copy: action('copy'),
+    }),
+    template,
+  }),
 });
 
-export const Default = Template.bind({});
-Default.args = {};
-Default.parameters = storyParametersObject(
-  Default.parameters,
-  template,
-  Default.args
-);
+export const Default = Template({});
